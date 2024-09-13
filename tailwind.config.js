3e7c86bf984c843e4bc86634366c0f4d8aa148e7
Armin Eslami <armineslami@hotmail.com>
2024-09-13 13:28:44 +0330

    Setup app theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
+import plugin from 'flowbite/plugin';
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -7,15 +7,112 @@ export default {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        './node_modules/flowbite/**/*.js',
     ],
 
     theme: {
         extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            colors: {
+                text: {
+                    50: 'var(--text-50)',
+                    100: 'var(--text-100)',
+                    200: 'var(--text-200)',
+                    300: 'var(--text-300)',
+                    400: 'var(--text-400)',
+                    500: 'var(--text-500)',
+                    600: 'var(--text-600)',
+                    700: 'var(--text-700)',
+                    800: 'var(--text-800)',
+                    900: 'var(--text-900)',
+                    950: 'var(--text-950)',
+                },
+                background: {
+                    50: 'var(--background-50)',
+                    100: 'var(--background-100)',
+                    200: 'var(--background-200)',
+                    300: 'var(--background-300)',
+                    400: 'var(--background-400)',
+                    500: 'var(--background-500)',
+                    600: 'var(--background-600)',
+                    700: 'var(--background-700)',
+                    800: 'var(--background-800)',
+                    900: 'var(--background-900)',
+                    950: 'var(--background-950)',
+                },
+                primary: {
+                    50: 'var(--primary-50)',
+                    100: 'var(--primary-100)',
+                    200: 'var(--primary-200)',
+                    300: 'var(--primary-300)',
+                    400: 'var(--primary-400)',
+                    500: 'var(--primary-500)',
+                    600: 'var(--primary-600)',
+                    700: 'var(--primary-700)',
+                    800: 'var(--primary-800)',
+                    900: 'var(--primary-900)',
+                    950: 'var(--primary-950)',
+                },
+                secondary: {
+                    50: 'var(--secondary-50)',
+                    100: 'var(--secondary-100)',
+                    200: 'var(--secondary-200)',
+                    300: 'var(--secondary-300)',
+                    400: 'var(--secondary-400)',
+                    500: 'var(--secondary-500)',
+                    600: 'var(--secondary-600)',
+                    700: 'var(--secondary-700)',
+                    800: 'var(--secondary-800)',
+                    900: 'var(--secondary-900)',
+                    950: 'var(--secondary-950)',
+                },
+                accent: {
+                    50: 'var(--accent-50)',
+                    100: 'var(--accent-100)',
+                    200: 'var(--accent-200)',
+                    300: 'var(--accent-300)',
+                    400: 'var(--accent-400)',
+                    500: 'var(--accent-500)',
+                    600: 'var(--accent-600)',
+                    700: 'var(--accent-700)',
+                    800: 'var(--accent-800)',
+                    900: 'var(--accent-900)',
+                    950: 'var(--accent-950)',
+                },
+                // primary: {
+                //     DEFAULT: "#f4be6a",  // Default primary color
+                //     light: "#efaa3f",    // Optional: Lighter shade
+                //     dark: "#f1c37c",     // Optional: Darker shade
+                // },
+                // secondary: {
+                //     DEFAULT: "#fbf4df",  // Default secondary color
+                //     light: "#fbf4df",    // Optional: Lighter shade
+                //     dark: "#546669",     // Optional: Darker shade
+                // },
+                // text: {
+                //     primary: "#3d392b",
+                //     secondary: "#534e3a",
+                //     trinary: "#858484"
+                // },
+                // button: {
+                //     primary: {
+                //         DEFAULT: "#fbf4df",
+                //         light: "#fbf4df",
+                //         dark: "#fbf4df",
+                //         hover: "#534e3a",
+                //     },
+                //     secondary: {
+                //         DEFAULT: "#fbf4df",
+                //         light: "#fbf4df",
+                //         dark: "#fbf4df",
+                //         hover: "#534e3a",
+                //     },
+                // }
             },
+            // fontFamily: {
+            //     sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            // },
         },
     },
 
-    plugins: [forms],
+    plugins: [forms, plugin],
 };
