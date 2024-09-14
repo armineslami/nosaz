5831a57aca545e48be41d8378928a10b5eb69825
Armin Eslami <armineslami@hotmail.com>
2024-09-14 09:09:02 +0330

    Add min height

--- resources/views/components/secondary-button.blade.php
+++ resources/views/components/secondary-button.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->merge(['type' => 'button', 'class' => 'secondary-button inline-flex items-center px-4 py-2 bg-secondary border border-transparent rounded-md font-semibold text-xs text-text uppercase shadow-sm focus:outline-none focus:ring-2 focus:ring-secondary-600 focus:ring-offset-secondary-600 dark:focus:ring-offset-secondary-600 disabled:opacity-25 transition ease-in-out duration-150']) }}>
+<button {{ $attributes->merge(['type' => 'button', 'class' => 'secondary-button min-h-8 inline-flex items-center px-4 py-2 bg-secondary border border-transparent rounded-md font-semibold text-xs text-text uppercase shadow-sm focus:outline-none focus:ring-2 focus:ring-secondary-600 focus:ring-offset-secondary-600 dark:focus:ring-offset-secondary-600 disabled:opacity-25 transition ease-in-out duration-150']) }}>
     {{ $slot }}
 </button>
