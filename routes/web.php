59ff620e03cc56428565dbf06d846e7f2cb866e9
Armin Eslami <armineslami@hotmail.com>
2024-09-23 19:24:35 +0330

    Create DashboardController.

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,17 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\FormulaController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return redirect('dashboard');
 });
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/', function () { return redirect('dashboard'); });
+    Route::get('/dashboard', DashboardController::class)->name('dashboard');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
