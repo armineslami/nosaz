4f5f33eb52cdf3e23057d9d98752a4937dbc7062
Armin Eslami <armineslami@hotmail.com>
2024-09-29 18:52:40 +0330

    Add variable repository and interface

--- app/Interfaces/VariableRepositoryInterface.php
+++ app/Interfaces/VariableRepositoryInterface.php
@@ -9,4 +9,6 @@ interface VariableRepositoryInterface
 {
     public static function byId($id, $private = false): ?Variable;
     public static function all($private = false): Collection;
+    public static function create(string $name, int $user_id = null): Variable;
+    public static function destroy(int $id, int $user_id): bool;
 }
--- app/Repositories/VariableRepository.php
+++ app/Repositories/VariableRepository.php
@@ -18,4 +18,21 @@ class VariableRepository implements VariableRepositoryInterface
     {
         return Auth::user()->variables()->get();
     }
+
+    public static function create(string $name, int $user_id = null): Variable
+    {
+        return Variable::create([
+            'name' => $name,
+            'user_id' => $user_id
+        ]);
+    }
+
+    public static function destroy(int $id, int $user_id): bool
+    {
+        $variable = self::byId($id);
+        if ($variable->user_id === $user_id) {
+            return Variable::destroy($id);
+        }
+        return false;
+    }
 }
