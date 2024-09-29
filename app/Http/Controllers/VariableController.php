b252e517279e8219ba50bc98bf4fe42d44f502b6
Armin Eslami <armineslami@hotmail.com>
2024-09-29 18:54:49 +0330

    Implement variable controller

---
+++ app/Http/Controllers/VariableController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\CreateVariableRequest;
+use App\Repositories\VariableRepository;
+use App\Services\Formula\FormulaService;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\View\View;
+
+class VariableController extends Controller
+{
+    public function create(): View
+    {
+        $variables = VariableRepository::all();
+        return view('formula.variable.create', ['variables' => $variables->all()]);
+    }
+
+    public function store(CreateVariableRequest $request)
+    {
+        FormulaService::CreateVariable($request->name, Auth::user()->id);
+        return Redirect::route('formula.variable.create')->with('status', 'variable_created');
+    }
+
+    public function destroy(Request $request): JsonResponse
+    {
+        $result = FormulaService::destroyVariable($request->id, Auth::user()->id);
+        return response()->json(['deleted' => $result ]);
+    }
+}
