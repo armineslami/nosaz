7e82702201d754603fa2045096f9b44e220087f5
Armin Eslami <armineslami@hotmail.com>
2024-09-29 20:23:07 +0330

    Add padding to variabe name.

--- resources/views/formula/variable/create.blade.php
+++ resources/views/formula/variable/create.blade.php
@@ -43,7 +43,7 @@
                     <div id="variablesContainer"  class="flex gap-4">
                         @foreach($variables as $variable)
                             <div class="flex justify-between items-center bg-secondary py-2 px-3 min-w-24 rounded-md text-text text-sm cursor-default">
-                                <p>{{ $variable->name }}</p>
+                                <p class="pe-4">{{ $variable->name }}</p>
                                 <div class="cursor-pointer deleteButton" data-id="{{ $variable->id }}">
                                     <svg class="size-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
