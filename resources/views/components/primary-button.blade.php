8edb376929a9759f7c3348c8e356acd72ee7c996
Armin Eslami <armineslami@hotmail.com>
2024-10-03 11:08:33 +0330

    Update min width

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'primary-button min-h-8 inline-flex items-center px-4 py-2 bg-primary border border-transparent rounded-md font-semibold text-xs text-text uppercase focus:outline-none focus:ring-2 focus:ring-primary-600 focus:ring-offset-2-primary-600 dark:focus:ring-offset-primary-600 transition ease-in-out duration-150']) }}>
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'primary-button h-8 inline-flex justify-center items-center px-4 py-2 min-w-24 bg-primary border border-primary-500 rounded-md font-semibold text-xs text-text uppercase shadow-sm focus:outline-none focus:ring-2 focus:ring-primary-600 focus:ring-offset-2-primary-600 dark:focus:ring-offset-primary-600 transition ease-in-out duration-150']) }}>
     {{ $slot }}
 </button>
