97dfe12a429f0bde36073b94b84734b191cbb360
Armin Eslami <armineslami@hotmail.com>
2024-10-03 19:12:51 +0330

    Update background color

--- resources/views/layouts/social-modal.blade.php
+++ resources/views/layouts/social-modal.blade.php
@@ -1,7 +1,7 @@
 <div id="social-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
     <div class="relative p-4 w-full max-w-md max-h-full">
         <!-- Modal content -->
-        <div class="relative bg-background rounded-lg shadow">
+        <div class="relative bg-white dark:bg-gray-700 rounded-lg shadow">
 
             <!-- Modal header -->
             <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
@@ -18,7 +18,7 @@
 
             <!-- Modal body -->
             <div class="p-4 md:p-5">
-                <p class="text-sm font-normal text-gray-500 dark:text-gray-400">
+                <p class="text-sm font-normal text-gray-900 dark:text-white">
                     {{ __('برای تماس می‌توانید از طریق شبکه‌های زیر اقدام کنید.') }}
                 </p>
                 <ul class="my-4 space-y-3 ltr">
