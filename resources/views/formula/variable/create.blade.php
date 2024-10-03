19a6c64c69d8563dabda7ae88bf22616ad5bd7f0
Armin Eslami <armineslami@hotmail.com>
2024-10-03 19:35:53 +0330

    Update variables bg color.

--- resources/views/formula/variable/create.blade.php
+++ resources/views/formula/variable/create.blade.php
@@ -42,7 +42,7 @@
                 @if(!empty($variables))
                     <div id="variablesContainer"  class="flex gap-4">
                         @foreach($variables as $variable)
-                            <div class="flex justify-between items-center bg-secondary py-2 px-3 min-w-24 rounded-md text-text text-sm cursor-default">
+                            <div class="flex justify-between items-center bg-primary py-2 px-3 min-w-24 rounded-md text-text text-sm cursor-default">
                                 <p class="pe-4">{{ $variable->name }}</p>
                                 <div class="cursor-pointer deleteButton" data-id="{{ $variable->id }}">
                                     <svg class="size-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
