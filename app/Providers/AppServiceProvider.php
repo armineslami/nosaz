1f6b6c2d7b6b40e60abf8682a43ba6366d82455a
Armin Eslami <armineslami@hotmail.com>
2024-10-07 20:46:32 +0330

    Modify verification email texts.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        VerifyEmail::toMailUsing(function (object $notifiable, string $url) {
+            return (new MailMessage)
+                ->subject('فعال سازی حساب کاری در ' . __('app.name'))
+                ->greeting('سلام')
+                ->line('این ایمیل جهت فعال سازی حساب کاربری شما ارسال شده است. برای انجام این کار کافیست روی دکمه زیر کلیک کنید.')
+                ->action('تایید ایمیل', $url)
+                ->salutation('تیم ' . __('app.name'));
+        });
     }
 }
