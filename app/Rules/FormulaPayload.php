b2fd7d18f669f92758027cea1960eff1d7ad1e59
Armin Eslami <armineslami@hotmail.com>
2024-10-08 15:50:44 +0330

    Create new rule for validating formula.

---
+++ app/Rules/FormulaPayload.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Rules;
+
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
+
+class FormulaPayload implements ValidationRule
+{
+    /**
+     * Run the validation rule.
+     *
+     * @param  \Closure(string, ?string = null): \Illuminate\Translation\PotentiallyTranslatedString  $fail
+     */
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        // Regular expression to match valid characters outside <>
+        // Allowed characters: 1234567890-+=)(*^%/#, and <>
+        $allowedCharactersPattern = '/^[0-9\-+=)(*^%\/#<>]*$/';
+
+        // Remove everything between <> for the check
+        // The pattern finds anything between <>
+        $outsideText = preg_replace('/<[^<>]*>/', '', $value);
+
+        // Check if the remaining text outside <> contains only allowed characters
+        if (!preg_match($allowedCharactersPattern, $outsideText)) {
+//            $fail('The :attribute contains invalid characters outside <>.');
+            $fail(trans('validation.custom.formula.invalid'));
+        }
+    }
+}
