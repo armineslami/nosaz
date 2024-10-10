087787bd797a62126d9f99b1c7a35a2e09220abd
Armin Eslami <armineslami@hotmail.com>
2024-10-10 17:22:50 +0330

    Add project sub menu.

--- resources/views/layouts/bottom-navigation.blade.php
+++ resources/views/layouts/bottom-navigation.blade.php
@@ -6,12 +6,27 @@
             </svg>
             <span class="text-xs {{ Route::is('dashboard') ? 'text-text-900 dark:text-text-100' : 'text-gray-900 dark:text-gray-100 group-hover:text-text-900 dark:group-hover:text-text-100' }}">{{ __('داشبورد') }}</span>
         </a>
-        <a href="{{ route('projects.edit') }}" type="button" class="inline-flex flex-col items-center justify-center px-5 group {{ Route::is('projects.edit') ? 'bg-primary' : 'hover:bg-primary' }}">
-            <svg class="w-5 h-5 mb-2 {{ Route::is('projects.edit') ? 'text-text-900 dark:text-text-100' : 'text-gray-900 dark:text-gray-100 group-hover:text-text-900 dark:group-hover:text-text-100' }}" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+        <a data-popover-target="project-menu-popover" href="{{ route('project.index') }}" type="button" class="inline-flex flex-col items-center justify-center px-5 group {{ Route::is('project.*') ? 'bg-primary' : 'hover:bg-primary' }}">
+            <svg class="w-5 h-5 mb-2 {{ Route::is('project.*') ? 'text-text-900 dark:text-text-100' : 'text-gray-900 dark:text-gray-100 group-hover:text-text-900 dark:group-hover:text-text-100' }}" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Z" />
             </svg>
-            <span class="text-xs {{ Route::is('projects') ? 'text-text-900 dark:text-text-100' : 'text-gray-900 dark:text-gray-100 group-hover:text-text-900 dark:group-hover:text-text-100' }}">{{ __('پروژه‌ها') }}</span>
+            <span class="text-xs {{ Route::is('project.*') ? 'text-text-900 dark:text-text-100' : 'text-gray-900 dark:text-gray-100 group-hover:text-text-900 dark:group-hover:text-text-100' }}">{{ __('پروژه‌ها') }}</span>
         </a>
+        <div data-popover id="project-menu-popover" role="tooltip" class="absolute z-11 invisible inline-block w-[fit-content] transition-opacity duration-300 bg-white dark:bg-gray-700  border border-gray-200 dark:border-gray-600 rounded-lg shadow-sm opacity-0">
+            <ul class="mb-2">
+                <li>
+                    <a href="{{ route('project.create') }}" class="flex text-sm w-ful px-4 py-2 text-gray-500 dark:text-gray-400 transition duration-75 rounded-md pl-11 group hover:bg-gray-100 dark:hover:bg-gray-700">
+                        {{ __('ساخت پروژه') }}
+                    </a>
+                </li>
+                <li>
+                    <a href="{{ route('project.index') }}" class="flex text-sm w-full px-4 py-2 text-gray-500 dark:text-gray-400 transition duration-75 rounded-md pl-11 group hover:bg-gray-100 dark:hover:bg-gray-700">
+                        {{ __('پروژه‌ها') }}
+                    </a>
+                </li>
+            </ul>
+            <div data-popper-arrow></div>
+        </div>
         <a data-popover-target="formula-menu-popover" href="{{ route('formula.index') }}" type="button" class="inline-flex flex-col items-center justify-center px-5 group {{ Route::is('formula.*') ? 'bg-primary' : 'hover:bg-primary' }}">
             <svg class="w-5 h-5 mb-2 {{ Route::is('formula.*') ? 'text-text-900 dark:text-text-100' : 'text-gray-900 dark:text-gray-100 group-hover:text-text-900 dark:group-hover:text-text-100' }}" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 3.104v5.714a2.25 2.25 0 0 1-.659 1.591L5 14.5M9.75 3.104c-.251.023-.501.05-.75.082m.75-.082a24.301 24.301 0 0 1 4.5 0m0 0v5.714c0 .597.237 1.17.659 1.591L19.8 15.3M14.25 3.104c.251.023.501.05.75.082M19.8 15.3l-1.57.393A9.065 9.065 0 0 1 12 15a9.065 9.065 0 0 0-6.23-.693L5 14.5m14.8.8 1.402 1.402c1.232 1.232.65 3.318-1.067 3.611A48.309 48.309 0 0 1 12 21c-2.773 0-5.491-.235-8.135-.687-1.718-.293-2.3-2.379-1.067-3.61L5 14.5" />
--- resources/views/layouts/drawer.blade.php
+++ resources/views/layouts/drawer.blade.php
@@ -12,67 +12,95 @@
             </div>
         </div>
 
-        <div class="mt-12">
-            <a
-                class="flex items-center px-4 py-2 mt-0 rounded-md {{ Route::is('dashboard') ? 'bg-primary text-text-900 dark:text-text-100' : 'primary-button text-gray-900 dark:text-gray-100 hover:bg-primary dark:hover:text-text-100 dark:hover:bg-primary' }}"
-                href="{{ route('dashboard') }}">
-                <svg class="size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6A2.25 2.25 0 0 1 6 3.75h2.25A2.25 2.25 0 0 1 10.5 6v2.25a2.25 2.25 0 0 1-2.25 2.25H6a2.25 2.25 0 0 1-2.25-2.25V6ZM3.75 15.75A2.25 2.25 0 0 1 6 13.5h2.25a2.25 2.25 0 0 1 2.25 2.25V18a2.25 2.25 0 0 1-2.25 2.25H6A2.25 2.25 0 0 1 3.75 18v-2.25ZM13.5 6a2.25 2.25 0 0 1 2.25-2.25H18A2.25 2.25 0 0 1 20.25 6v2.25A2.25 2.25 0 0 1 18 10.5h-2.25a2.25 2.25 0 0 1-2.25-2.25V6ZM13.5 15.75a2.25 2.25 0 0 1 2.25-2.25H18a2.25 2.25 0 0 1 2.25 2.25V18A2.25 2.25 0 0 1 18 20.25h-2.25A2.25 2.25 0 0 1 13.5 18v-2.25Z" />
-                </svg>
-                <span class="ms-4 text-sm">{{ __('داشبورد') }}</span>
-            </a>
-            <a
-                class="flex items-center px-4 py-2 mt-4 rounded-md {{ Route::is('projects.edit') ? 'primary-button bg-primary text-text-900 dark:text-text-100' : 'primary-button text-gray-900 dark:text-gray-100 hover:bg-primary dark:hover:text-text-100 dark:hover:bg-primary' }}"
-                href="{{ route('projects.edit') }}">
-                <svg class="size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Z" />
-                </svg>
-                <span class="ms-4 text-sm">{{ __('پروژه‌ها') }}</span>
-            </a>
-            <div>
-                <div
-                    aria-controls="formula-dropdown" data-collapse-toggle="formula-dropdown"
-                    class="flex cursor-pointer items-center px-4 py-2 mt-4 rounded-md {{ Route::is('formula.*') ? 'primary-button bg-primary text-text-900 dark:text-text-100' : 'primary-button text-gray-900 dark:text-gray-100 hover:bg-primary dark:hover:text-text-100 dark:hover:bg-primary' }}">
-                    <svg class="size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 3.104v5.714a2.25 2.25 0 0 1-.659 1.591L5 14.5M9.75 3.104c-.251.023-.501.05-.75.082m.75-.082a24.301 24.301 0 0 1 4.5 0m0 0v5.714c0 .597.237 1.17.659 1.591L19.8 15.3M14.25 3.104c.251.023.501.05.75.082M19.8 15.3l-1.57.393A9.065 9.065 0 0 1 12 15a9.065 9.065 0 0 0-6.23-.693L5 14.5m14.8.8 1.402 1.402c1.232 1.232.65 3.318-1.067 3.611A48.309 48.309 0 0 1 12 21c-2.773 0-5.491-.235-8.135-.687-1.718-.293-2.3-2.379-1.067-3.61L5 14.5" />
-                    </svg>
-                    <span class="ms-4 text-sm">{{ __('فرمول‌ها') }}</span>
-                    <span class="bg-accent rounded border-0 text-xsmall px-1 py-0.5 ms-2 text-white font-bold">{{ __('آزمایشی') }}</span>
-                    <svg class="w-2 h-2 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="m1 1 4 4 4-4"/>
-                    </svg>
-                </div>
-                <ul id="formula-dropdown" class="{{ Route::is('formula.*') ? '' : 'hidden' }} py-2 space-y-2 mt-2">
-                    <li>
-                        <a href="{{ route('formula.create') }}" class="flex text-sm w-full ms-4 px-4 py-2 text-gray-500 dark:text-gray-400 transition duration-75 rounded-md pl-11 group hover:bg-gray-100 dark:hover:bg-gray-700">
-                            {{ __('ساخت فرمول') }}
-                        </a>
-                    </li>
-                    <li>
-                        <a href="{{ route('formula.index') }}" class="flex text-sm w-full ms-4 px-4 py-2 text-gray-500 dark:text-gray-400 transition duration-75 rounded-md pl-11 group hover:bg-gray-100 dark:hover:bg-gray-700">
-                            {{ __('فرمول‌ها') }}
-                        </a>
-                    </li>
-                    <li>
-                        <a href="{{ route('formula.variable.create') }}" class="flex text-sm w-full ms-4 px-4 py-2 text-gray-500 dark:text-gray-400 transition duration-75 rounded-md pl-11 group hover:bg-gray-100 dark:hover:bg-gray-700">
-                            {{ __('متغیرها') }}
-                        </a>
-                    </li>
-                </ul>
-            </div>
-            <a
-                class="flex items-center px-4 py-2 mt-4 rounded-md {{ Route::is('reports') ? 'primary-button flex bg-primary text-text-900 dark:text-text-100' : 'primary-button text-gray-900 dark:text-gray-100 hover:bg-primary dark:hover:text-text-100 dark:hover:bg-primary' }}"
-                href="{{ route('reports') }}">
-                <svg  class="size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 6a7.5 7.5 0 1 0 7.5 7.5h-7.5V6Z" />
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 10.5H21A7.5 7.5 0 0 0 13.5 3v7.5Z" />
-                </svg>
-                <span class="ms-4 text-sm">{{ __('گزارشات') }}</span>
-            </a>
+        <div class="mt-8">
+           <div class="overflow-y-scroll">
+               <a
+                       class="flex items-center px-4 py-2 mt-0 rounded-md {{ Route::is('dashboard') ? 'bg-primary text-text-900 dark:text-text-100' : 'primary-button text-gray-900 dark:text-gray-100 hover:bg-primary dark:hover:text-text-100 dark:hover:bg-primary' }}"
+                       href="{{ route('dashboard') }}">
+                   <svg class="size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                       <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6A2.25 2.25 0 0 1 6 3.75h2.25A2.25 2.25 0 0 1 10.5 6v2.25a2.25 2.25 0 0 1-2.25 2.25H6a2.25 2.25 0 0 1-2.25-2.25V6ZM3.75 15.75A2.25 2.25 0 0 1 6 13.5h2.25a2.25 2.25 0 0 1 2.25 2.25V18a2.25 2.25 0 0 1-2.25 2.25H6A2.25 2.25 0 0 1 3.75 18v-2.25ZM13.5 6a2.25 2.25 0 0 1 2.25-2.25H18A2.25 2.25 0 0 1 20.25 6v2.25A2.25 2.25 0 0 1 18 10.5h-2.25a2.25 2.25 0 0 1-2.25-2.25V6ZM13.5 15.75a2.25 2.25 0 0 1 2.25-2.25H18a2.25 2.25 0 0 1 2.25 2.25V18A2.25 2.25 0 0 1 18 20.25h-2.25A2.25 2.25 0 0 1 13.5 18v-2.25Z" />
+                   </svg>
+                   <span class="ms-4 text-sm">{{ __('داشبورد') }}</span>
+               </a>
+               {{--            <a--}}
+               {{--                class="flex items-center px-4 py-2 mt-4 rounded-md {{ Route::is('project.*') ? 'primary-button bg-primary text-text-900 dark:text-text-100' : 'primary-button text-gray-900 dark:text-gray-100 hover:bg-primary dark:hover:text-text-100 dark:hover:bg-primary' }}"--}}
+               {{--                href="{{ route('project.index') }}">--}}
+               {{--                <svg class="size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">--}}
+               {{--                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Z" />--}}
+               {{--                </svg>--}}
+               {{--                <span class="ms-4 text-sm">{{ __('پروژه‌ها') }}</span>--}}
+               {{--            </a>--}}
+
+               <div>
+                   <div
+                           aria-controls="project-dropdown" data-collapse-toggle="project-dropdown"
+                           class="flex relative cursor-pointer items-center px-4 py-2 mt-4 rounded-md {{ Route::is('project.*') ? 'primary-button bg-primary text-text-900 dark:text-text-100' : 'primary-button text-gray-900 dark:text-gray-100 hover:bg-primary dark:hover:text-text-100 dark:hover:bg-primary' }}">
+                       <svg class="size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                           <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Z" />
+                       </svg>
+                       <span class="ms-4 text-sm">{{ __('پروژه‌ها') }}</span>
+                       <svg class="w-2 h-2 absolute end-4 bottom-0 top-0 m-auto" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                           <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="m1 1 4 4 4-4"/>
+                       </svg>
+                   </div>
+                   <ul id="project-dropdown" class="{{ Route::is('project.*') ? '' : 'hidden' }} py-2 space-y-1 mt-1">
+                       <li>
+                           <a href="{{ route('project.create') }}" class="flex text-xs w-full ms-4 px-4 py-2 text-gray-500 dark:text-gray-400 transition duration-75 rounded-md pl-11 group hover:bg-gray-100 dark:hover:bg-gray-700">
+                               {{ __('ساخت پروژه') }}
+                           </a>
+                       </li>
+                       <li>
+                           <a href="{{ route('project.index') }}" class="flex text-xs w-full ms-4 px-4 py-2 text-gray-500 dark:text-gray-400 transition duration-75 rounded-md pl-11 group hover:bg-gray-100 dark:hover:bg-gray-700">
+                               {{ __('پروژه‌ها') }}
+                           </a>
+                       </li>
+                   </ul>
+               </div>
+               <div>
+                   <div
+                           aria-controls="formula-dropdown" data-collapse-toggle="formula-dropdown"
+                           class="flex cursor-pointer items-center px-4 py-2 mt-4 rounded-md {{ Route::is('formula.*') ? 'primary-button bg-primary text-text-900 dark:text-text-100' : 'primary-button text-gray-900 dark:text-gray-100 hover:bg-primary dark:hover:text-text-100 dark:hover:bg-primary' }}">
+                       <svg class="size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                           <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 3.104v5.714a2.25 2.25 0 0 1-.659 1.591L5 14.5M9.75 3.104c-.251.023-.501.05-.75.082m.75-.082a24.301 24.301 0 0 1 4.5 0m0 0v5.714c0 .597.237 1.17.659 1.591L19.8 15.3M14.25 3.104c.251.023.501.05.75.082M19.8 15.3l-1.57.393A9.065 9.065 0 0 1 12 15a9.065 9.065 0 0 0-6.23-.693L5 14.5m14.8.8 1.402 1.402c1.232 1.232.65 3.318-1.067 3.611A48.309 48.309 0 0 1 12 21c-2.773 0-5.491-.235-8.135-.687-1.718-.293-2.3-2.379-1.067-3.61L5 14.5" />
+                       </svg>
+                       <span class="ms-4 text-sm">{{ __('فرمول‌ها') }}</span>
+                       <span class="bg-accent rounded border-0 text-xsmall px-1 py-0.5 ms-2 text-white font-bold">{{ __('آزمایشی') }}</span>
+                       <svg class="w-2 h-2 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                           <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="m1 1 4 4 4-4"/>
+                       </svg>
+                   </div>
+                   <ul id="formula-dropdown" class="{{ Route::is('formula.*') ? '' : 'hidden' }} py-2 space-y-1 mt-1">
+                       <li>
+                           <a href="{{ route('formula.create') }}" class="flex text-xs w-full ms-4 px-4 py-2 text-gray-500 dark:text-gray-400 transition duration-75 rounded-md pl-11 group hover:bg-gray-100 dark:hover:bg-gray-700">
+                               {{ __('ساخت فرمول') }}
+                           </a>
+                       </li>
+                       <li>
+                           <a href="{{ route('formula.index') }}" class="flex text-xs w-full ms-4 px-4 py-2 text-gray-500 dark:text-gray-400 transition duration-75 rounded-md pl-11 group hover:bg-gray-100 dark:hover:bg-gray-700">
+                               {{ __('فرمول‌ها') }}
+                           </a>
+                       </li>
+                       <li>
+                           <a href="{{ route('formula.variable.create') }}" class="flex text-xs w-full ms-4 px-4 py-2 text-gray-500 dark:text-gray-400 transition duration-75 rounded-md pl-11 group hover:bg-gray-100 dark:hover:bg-gray-700">
+                               {{ __('متغیرها') }}
+                           </a>
+                       </li>
+                   </ul>
+               </div>
+               <a
+                       class="flex items-center px-4 py-2 mt-4 rounded-md {{ Route::is('reports') ? 'primary-button flex bg-primary text-text-900 dark:text-text-100' : 'primary-button text-gray-900 dark:text-gray-100 hover:bg-primary dark:hover:text-text-100 dark:hover:bg-primary' }}"
+                       href="{{ route('reports') }}">
+                   <svg  class="size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                       <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 6a7.5 7.5 0 1 0 7.5 7.5h-7.5V6Z" />
+                       <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 10.5H21A7.5 7.5 0 0 0 13.5 3v7.5Z" />
+                   </svg>
+                   <span class="ms-4 text-sm">{{ __('گزارشات') }}</span>
+               </a>
+           </div>
 
             <div class="absolute bottom-0 left-0 right-0">
 
-                <div class="mb-8">
+                <div class="mb-4">
                     <a
                         class="flex items-center px-4 py-2 mt-4 rounded-md text-gray-500 dark:text-gray-600"
                         target="_blank"
@@ -98,7 +126,7 @@
                 <div
                     data-modal-target="social-modal" data-modal-toggle="social-modal"
                     id="effect-area"
-                    class="relative text-gray-500 dark:text-gray-600 text-center text-xs mb-2 cursor-pointer">
+                    class="relative text-gray-500 dark:text-gray-600 text-center text-xs mb-0 cursor-pointer">
                     <div class="relative text-center inline-flex">
                         <span>✨</span>
                         <span class="font-bold mx-1" >Armin</span>
