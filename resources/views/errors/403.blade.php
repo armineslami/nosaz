6b93820ced84cfcd7d09c1fe3a0e8c8c97674437
Armin Eslami <armineslami@hotmail.com>
2024-10-11 21:40:31 +0330

    Only use the translation as the message.

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -2,4 +2,4 @@
 
 @section('title', __('errors.forbidden'))
 @section('code', '403')
-@section('message', __($exception->getMessage() ?: 'errors.forbidden'))
+@section('message', __('errors.forbidden'))
