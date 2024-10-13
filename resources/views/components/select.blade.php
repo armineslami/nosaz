26bc586149486728a7cd12b0afd5728ee4c49aec
Armin Eslami <armineslami@hotmail.com>
2024-10-13 22:38:19 +0330

    Add select component.

---
+++ resources/views/components/select.blade.php
@@ -0,0 +1,4 @@
+<select {{ $attributes->merge(['class' => 'text-sm border-gray-300 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-300 focus:border-accent dark:focus:border-accent focus:ring-accent dark:focus:ring-accent rounded-md shadow-sm']) }}>
+        }>
+    {{ $slot }}
+</select>
