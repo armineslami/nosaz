c136eff50698c9317b7495a993050c0452057fb9
Armin Eslami <armineslami@hotmail.com>
2024-10-15 08:59:52 +0330

    Load KUTE with delay to avoid freez on page load.

--- resources/views/formula/empty.blade.php
+++ resources/views/formula/empty.blade.php
@@ -1,40 +1,59 @@
 <div class="flex items-center m-auto mt-20 md:mt-32 text-center rounded-lg">
     <div class="w-full">
         <div class="relative mb-32">
-            <svg class="absolute top-0 left-0 bottom-0 -right-16 md:right-0 m-auto fill-gray-300 dark:fill-gray-800 opacity-20" id="visual" viewBox="0 0 900 600" width="450" height="300">
+            <svg class="absolute top-0 left-0 bottom-0 -right-16 md:right-0 m-auto fill-gray-300 dark:fill-gray-800 opacity-20"
+                id="visual" viewBox="0 0 900 600" width="450" height="300">
                 <g transform="translate(415.80122448615737 318.1032235833984)">
-                    <path id="blob1" d="M173 -164.4C223 -123 261.5 -61.5 257.5 -4C253.5 53.5 207 107 157 141.2C107 175.3 53.5 190.2 -2.5 192.6C-58.5 195.1 -116.9 185.2 -148.2 151.1C-179.4 116.9 -183.5 58.5 -187.1 -3.7C-190.8 -65.8 -194 -131.5 -162.8 -172.9C-131.5 -214.2 -65.8 -231.1 -2.1 -229C61.5 -226.9 123 -205.7 173 -164.4"></path>
+                    <path id="blob1"
+                        d="M173 -164.4C223 -123 261.5 -61.5 257.5 -4C253.5 53.5 207 107 157 141.2C107 175.3 53.5 190.2 -2.5 192.6C-58.5 195.1 -116.9 185.2 -148.2 151.1C-179.4 116.9 -183.5 58.5 -187.1 -3.7C-190.8 -65.8 -194 -131.5 -162.8 -172.9C-131.5 -214.2 -65.8 -231.1 -2.1 -229C61.5 -226.9 123 -205.7 173 -164.4">
+                    </path>
                 </g>
                 <g class="hidden" transform="translate(454.2142609407367 284.35799449039416)">
-                    <path id="blob2" d="M145.7 -126.9C195.7 -95.7 247.8 -47.8 247.8 0C247.8 47.8 195.7 95.7 145.7 139.2C95.7 182.7 47.8 221.8 1.8 220.1C-44.3 218.3 -88.6 175.6 -138.3 132.1C-188 88.6 -243 44.3 -254.1 -11.1C-265.1 -66.5 -232.3 -132.9 -182.6 -164.2C-132.9 -195.4 -66.5 -191.5 -9.3 -182.2C47.8 -172.9 95.7 -158.2 145.7 -126.9"></path>
+                    <path id="blob2"
+                        d="M145.7 -126.9C195.7 -95.7 247.8 -47.8 247.8 0C247.8 47.8 195.7 95.7 145.7 139.2C95.7 182.7 47.8 221.8 1.8 220.1C-44.3 218.3 -88.6 175.6 -138.3 132.1C-188 88.6 -243 44.3 -254.1 -11.1C-265.1 -66.5 -232.3 -132.9 -182.6 -164.2C-132.9 -195.4 -66.5 -191.5 -9.3 -182.2C47.8 -172.9 95.7 -158.2 145.7 -126.9">
+                    </path>
                 </g>
             </svg>
-            <svg class="absolute top-0 left-0 bottom-0 -right-5 md:right-0 m-auto fill-gray-300 dark:fill-gray-400 opacity-40" id="visual" viewBox="0 0 900 600" width="300" height="200">
+            <svg class="absolute top-0 left-0 bottom-0 -right-5 md:right-0 m-auto fill-gray-300 dark:fill-gray-400 opacity-40"
+                id="visual" viewBox="0 0 900 600" width="300" height="200">
                 <g transform="translate(415.80122448615737 318.1032235833984)">
-                    <path id="blob3" d="M173 -164.4C223 -123 261.5 -61.5 257.5 -4C253.5 53.5 207 107 157 141.2C107 175.3 53.5 190.2 -2.5 192.6C-58.5 195.1 -116.9 185.2 -148.2 151.1C-179.4 116.9 -183.5 58.5 -187.1 -3.7C-190.8 -65.8 -194 -131.5 -162.8 -172.9C-131.5 -214.2 -65.8 -231.1 -2.1 -229C61.5 -226.9 123 -205.7 173 -164.4"></path>
+                    <path id="blob3"
+                        d="M173 -164.4C223 -123 261.5 -61.5 257.5 -4C253.5 53.5 207 107 157 141.2C107 175.3 53.5 190.2 -2.5 192.6C-58.5 195.1 -116.9 185.2 -148.2 151.1C-179.4 116.9 -183.5 58.5 -187.1 -3.7C-190.8 -65.8 -194 -131.5 -162.8 -172.9C-131.5 -214.2 -65.8 -231.1 -2.1 -229C61.5 -226.9 123 -205.7 173 -164.4">
+                    </path>
                 </g>
                 <g class="hidden" transform="translate(454.2142609407367 284.35799449039416)">
-                    <path id="blob4" d="M145.7 -126.9C195.7 -95.7 247.8 -47.8 247.8 0C247.8 47.8 195.7 95.7 145.7 139.2C95.7 182.7 47.8 221.8 1.8 220.1C-44.3 218.3 -88.6 175.6 -138.3 132.1C-188 88.6 -243 44.3 -254.1 -11.1C-265.1 -66.5 -232.3 -132.9 -182.6 -164.2C-132.9 -195.4 -66.5 -191.5 -9.3 -182.2C47.8 -172.9 95.7 -158.2 145.7 -126.9"></path>
+                    <path id="blob4"
+                        d="M145.7 -126.9C195.7 -95.7 247.8 -47.8 247.8 0C247.8 47.8 195.7 95.7 145.7 139.2C95.7 182.7 47.8 221.8 1.8 220.1C-44.3 218.3 -88.6 175.6 -138.3 132.1C-188 88.6 -243 44.3 -254.1 -11.1C-265.1 -66.5 -232.3 -132.9 -182.6 -164.2C-132.9 -195.4 -66.5 -191.5 -9.3 -182.2C47.8 -172.9 95.7 -158.2 145.7 -126.9">
+                    </path>
                 </g>
             </svg>
-            <svg class="absolute top-0 left-0 bottom-0 right-0 m-auto fill-gray-300 dark:fill-gray-300 opacity-60" id="visual" viewBox="0 0 900 600" width="200" height="150">
+            <svg class="absolute top-0 left-0 bottom-0 right-0 m-auto fill-gray-300 dark:fill-gray-300 opacity-60"
+                id="visual" viewBox="0 0 900 600" width="200" height="150">
                 <g transform="translate(415.80122448615737 318.1032235833984)">
-                    <path id="blob5" d="M173 -164.4C223 -123 261.5 -61.5 257.5 -4C253.5 53.5 207 107 157 141.2C107 175.3 53.5 190.2 -2.5 192.6C-58.5 195.1 -116.9 185.2 -148.2 151.1C-179.4 116.9 -183.5 58.5 -187.1 -3.7C-190.8 -65.8 -194 -131.5 -162.8 -172.9C-131.5 -214.2 -65.8 -231.1 -2.1 -229C61.5 -226.9 123 -205.7 173 -164.4"></path>
+                    <path id="blob5"
+                        d="M173 -164.4C223 -123 261.5 -61.5 257.5 -4C253.5 53.5 207 107 157 141.2C107 175.3 53.5 190.2 -2.5 192.6C-58.5 195.1 -116.9 185.2 -148.2 151.1C-179.4 116.9 -183.5 58.5 -187.1 -3.7C-190.8 -65.8 -194 -131.5 -162.8 -172.9C-131.5 -214.2 -65.8 -231.1 -2.1 -229C61.5 -226.9 123 -205.7 173 -164.4">
+                    </path>
                 </g>
                 <g class="hidden" transform="translate(454.2142609407367 284.35799449039416)">
-                    <path id="blob6" d="M145.7 -126.9C195.7 -95.7 247.8 -47.8 247.8 0C247.8 47.8 195.7 95.7 145.7 139.2C95.7 182.7 47.8 221.8 1.8 220.1C-44.3 218.3 -88.6 175.6 -138.3 132.1C-188 88.6 -243 44.3 -254.1 -11.1C-265.1 -66.5 -232.3 -132.9 -182.6 -164.2C-132.9 -195.4 -66.5 -191.5 -9.3 -182.2C47.8 -172.9 95.7 -158.2 145.7 -126.9"></path>
+                    <path id="blob6"
+                        d="M145.7 -126.9C195.7 -95.7 247.8 -47.8 247.8 0C247.8 47.8 195.7 95.7 145.7 139.2C95.7 182.7 47.8 221.8 1.8 220.1C-44.3 218.3 -88.6 175.6 -138.3 132.1C-188 88.6 -243 44.3 -254.1 -11.1C-265.1 -66.5 -232.3 -132.9 -182.6 -164.2C-132.9 -195.4 -66.5 -191.5 -9.3 -182.2C47.8 -172.9 95.7 -158.2 145.7 -126.9">
+                    </path>
                 </g>
             </svg>
             <div id="formula-sparkle-area" class="absolute top-0 left-0 bottom-0 right-0 m-auto text-lg size-10">
-                <svg class="size-9 md:size-10" fill="none" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 3.104v5.714a2.25 2.25 0 0 1-.659 1.591L5 14.5M9.75 3.104c-.251.023-.501.05-.75.082m.75-.082a24.301 24.301 0 0 1 4.5 0m0 0v5.714c0 .597.237 1.17.659 1.591L19.8 15.3M14.25 3.104c.251.023.501.05.75.082M19.8 15.3l-1.57.393A9.065 9.065 0 0 1 12 15a9.065 9.065 0 0 0-6.23-.693L5 14.5m14.8.8 1.402 1.402c1.232 1.232.65 3.318-1.067 3.611A48.309 48.309 0 0 1 12 21c-2.773 0-5.491-.235-8.135-.687-1.718-.293-2.3-2.379-1.067-3.61L5 14.5" />
+                <svg class="size-9 md:size-10" fill="none" viewBox="0 0 24 24" stroke-width="1"
+                    stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M9.75 3.104v5.714a2.25 2.25 0 0 1-.659 1.591L5 14.5M9.75 3.104c-.251.023-.501.05-.75.082m.75-.082a24.301 24.301 0 0 1 4.5 0m0 0v5.714c0 .597.237 1.17.659 1.591L19.8 15.3M14.25 3.104c.251.023.501.05.75.082M19.8 15.3l-1.57.393A9.065 9.065 0 0 1 12 15a9.065 9.065 0 0 0-6.23-.693L5 14.5m14.8.8 1.402 1.402c1.232 1.232.65 3.318-1.067 3.611A48.309 48.309 0 0 1 12 21c-2.773 0-5.491-.235-8.135-.687-1.718-.293-2.3-2.379-1.067-3.61L5 14.5" />
                 </svg>
             </div>
         </div>
 
         <div>
             <p class="text-text text-sm font-bold mb-2">{{ __('فرمول خود را برای محاسبه بسازید') }}</p>
-            <p class="text-text-600 dark:text-gray-400 text-sm justify-center">{{ __('اگر می‌خواهید روش محاسبات خود را داشته باشید، می‌توانید فرمول خود را ساخته و از آن برای محاسبات پروژه استفاده کنید') }}</p>
+            <p class="text-text-600 dark:text-gray-400 text-sm justify-center">
+                {{ __('اگر می‌خواهید روش محاسبات خود را داشته باشید، می‌توانید فرمول خود را ساخته و از آن برای محاسبات پروژه استفاده کنید') }}
+            </p>
             <a href="{{ route('formula.create') }}">
                 <x-primary-button class="mt-4">
                     {{ __('ساخت فرمول') }}
@@ -45,25 +64,6 @@
 </div>
 <script>
     document.addEventListener('DOMContentLoaded', function() {
-        window.KUTE.fromTo(
-            '#blob1',
-            { path: '#blob1' },
-            { path: '#blob2' },
-            { repeat: 999, duration: 4000, yoyo: true }
-        ).start(null);
-        window.KUTE.fromTo(
-            '#blob3',
-            { path: '#blob3' },
-            { path: '#blob4' },
-            { repeat: 999, duration: 3500, yoyo: true }
-        ).start(null);
-        window.KUTE.fromTo(
-            '#blob5',
-            { path: '#blob5' },
-            { path: '#blob6' },
-            { repeat: 999, duration: 3000, yoyo: true }
-        ).start(null);
-
         const mainArea = document.getElementById('formula-sparkle-area');
 
         // Generate a sparkle with delay
@@ -78,7 +78,7 @@
             const emojis = ['✨', '💥'];
             const sparkle = document.createElement('span');
             sparkle.classList.add('sparkle');
-            sparkle.textContent = emojis[Math.round(Math.random() * emojis.length -1)];
+            sparkle.textContent = emojis[Math.round(Math.random() * emojis.length - 1)];
 
             // Set the position of the sparkle relative to the main star
             sparkle.style.left = '-4px';
@@ -98,6 +98,43 @@
                 sparkle.remove();
             }, 1500); // 1.5 seconds to disappear
         }
+
+        setTimeout(loadKute, 500);
+
+        function loadKute() {
+            window.KUTE.fromTo(
+                '#blob1', {
+                    path: '#blob1'
+                }, {
+                    path: '#blob2'
+                }, {
+                    repeat: 999,
+                    duration: 4000,
+                    yoyo: true
+                }
+            ).start(null);
+            window.KUTE.fromTo(
+                '#blob3', {
+                    path: '#blob3'
+                }, {
+                    path: '#blob4'
+                }, {
+                    repeat: 999,
+                    duration: 3500,
+                    yoyo: true
+                }
+            ).start(null);
+            window.KUTE.fromTo(
+                '#blob5', {
+                    path: '#blob5'
+                }, {
+                    path: '#blob6'
+                }, {
+                    repeat: 999,
+                    duration: 3000,
+                    yoyo: true
+                }
+            ).start(null);
+        }
     });
 </script>
-
