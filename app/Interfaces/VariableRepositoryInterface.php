d86103a9d994f7a01a394875e6c8ff2c72abc13a
Armin Eslami <armineslami@hotmail.com>
2024-10-15 16:33:29 +0330

    Add two new methods: 1-Method 'defaults' to get all default variables. 2-Method 'idIn' to get variabels from given array of ids.

--- app/Interfaces/VariableRepositoryInterface.php
+++ app/Interfaces/VariableRepositoryInterface.php
@@ -7,8 +7,9 @@ use Illuminate\Database\Eloquent\Collection;
 
 interface VariableRepositoryInterface
 {
-    public static function byId($id, $private = false): ?Variable;
-    public static function all($private = false): Collection;
+    public static function byId($id): ?Variable;
+    public static function all(): Collection;
+    public static function defaults(): Collection;
     public static function create(string $name, int $user_id = null): Variable;
     public static function destroy(int $id, int $user_id): bool;
 }
--- app/Repositories/VariableRepository.php
+++ app/Repositories/VariableRepository.php
@@ -4,19 +4,30 @@ namespace App\Repositories;
 
 use App\Interfaces\VariableRepositoryInterface;
 use App\Models\Variable;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Facades\Auth;
 
 class VariableRepository implements VariableRepositoryInterface
 {
-    public static function byId($id, $private = false): ?Variable
+    public static function byId($id): ?Variable
     {
         return Auth::user()->variables()->where('id', $id)->first();
     }
 
-    public static function all($private = false): Collection
+    public static function all(): Collection
     {
         return Auth::user()->variables()->get();
+    }
+
+    public static function defaults(): Collection
+    {
+        return Variable::where(['user_id' => null, 'created_at' => Carbon::create(1990, 1, 1)])->get();
+    }
+
+    public static function idIn(array $ids): Collection
+    {
+        return Auth::user()->variables()->whereIn('id', $ids)->get();
     }
 
     public static function create(string $name, int $user_id = null): Variable
