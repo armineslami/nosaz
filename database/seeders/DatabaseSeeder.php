785764ff9ced9e58b8a4086f1077ac0a5d941525
Armin Eslami <armineslami@hotmail.com>
2024-10-15 16:26:54 +0330

    Update default varibales name

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ use App\Models\Variable;
 use App\Models\Setting;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,8 +16,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        self::createUsers();
-        self::createSettings();
+        // self::createUsers();
+        // self::createSettings();
         self::createVariables();
     }
 
@@ -36,21 +37,22 @@ class DatabaseSeeder extends Seeder
 
     static function createVariables(): void
     {
-        Variable::create(['name' => __('variables.land_area')]);
-        Variable::create(['name' => __('variables.density_percentage')]);
-        Variable::create(['name' => __('variables.number_of_floor')]);
-        Variable::create(['name' => __('variables.number_of_units_per_floor')]);
-        Variable::create(['name' => __('variables.number_of_warehouses')]);
-        Variable::create(['name' => __('variables.area_of_warehouse')]);
-        Variable::create(['name' => __('variables.cost_per_meter')]);
-        Variable::create(['name' => __('variables.selling_price_per_meter')]);
-        Variable::create(['name' => __('variables.illegal_area_of_each_floor')]);
-        Variable::create(['name' => __('variables.cost_per_illegal_meter')]);
-        Variable::create(['name' => __('variables.constructor_share_percentage')]);
-        Variable::create(['name' => __('variables.over')]);
-        Variable::create(['name' => __('variables.land_purchase_price_per_meter')]);
-        Variable::create(['name' => __('variables.land_price')]);
-        Variable::create(['name' => __('variables.real_estate_commission_fee')]);
-        Variable::create(['name' => __('variables.other_expenses')]);
+        Variable::create(['name' => __('variables.land_area'), 'created_at' => Carbon::create(1990, 1, 1), 'updated_at' => Carbon::create(1990, 1, 1)]);
+        Variable::create(['name' => __('variables.density_percentage'), 'created_at' => Carbon::create(1990, 1, 1), 'updated_at' => Carbon::create(1990, 1, 1)]);
+        Variable::create(['name' => __('variables.illegal_density_percentage'), 'created_at' => Carbon::create(1990, 1, 1), 'updated_at' => Carbon::create(1990, 1, 1)]);
+        Variable::create(['name' => __('variables.number_of_floor'), 'created_at' => Carbon::create(1990, 1, 1), 'updated_at' => Carbon::create(1990, 1, 1)]);
+        Variable::create(['name' => __('variables.number_of_units_per_floor'), 'created_at' => Carbon::create(1990, 1, 1), 'updated_at' => Carbon::create(1990, 1, 1)]);
+        Variable::create(['name' => __('variables.number_of_warehouses'), 'created_at' => Carbon::create(1990, 1, 1), 'updated_at' => Carbon::create(1990, 1, 1)]);
+        Variable::create(['name' => __('variables.area_of_each_warehouse'), 'created_at' => Carbon::create(1990, 1, 1), 'updated_at' => Carbon::create(1990, 1, 1)]);
+        Variable::create(['name' => __('variables.construction_cost_per_meter'), 'created_at' => Carbon::create(1990, 1, 1), 'updated_at' => Carbon::create(1990, 1, 1)]);
+        Variable::create(['name' => __('variables.selling_price_per_meter'), 'created_at' => Carbon::create(1990, 1, 1), 'updated_at' => Carbon::create(1990, 1, 1)]);
+        Variable::create(['name' => __('variables.illegal_area_per_floor'), 'created_at' => Carbon::create(1990, 1, 1), 'updated_at' => Carbon::create(1990, 1, 1)]);
+        Variable::create(['name' => __('variables.cost_per_illegal_meter'), 'created_at' => Carbon::create(1990, 1, 1), 'updated_at' => Carbon::create(1990, 1, 1)]);
+        Variable::create(['name' => __('variables.builder_percentage'), 'created_at' => Carbon::create(1990, 1, 1), 'updated_at' => Carbon::create(1990, 1, 1)]);
+        Variable::create(['name' => __('variables.over'), 'created_at' => Carbon::create(1990, 1, 1), 'updated_at' => Carbon::create(1990, 1, 1)]);
+        Variable::create(['name' => __('variables.land_purchase_price_per_meter'), 'created_at' => Carbon::create(1990, 1, 1), 'updated_at' => Carbon::create(1990, 1, 1)]);
+        Variable::create(['name' => __('variables.land_price'), 'created_at' => Carbon::create(1990, 1, 1), 'updated_at' => Carbon::create(1990, 1, 1)]);
+        Variable::create(['name' => __('variables.real_estate_commission_fee'), 'created_at' => Carbon::create(1990, 1, 1), 'updated_at' => Carbon::create(1990, 1, 1)]);
+        Variable::create(['name' => __('variables.other_expenses'), 'created_at' => Carbon::create(1990, 1, 1), 'updated_at' => Carbon::create(1990, 1, 1)]);
     }
 }
