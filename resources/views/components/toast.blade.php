ea7522944d75150d5a0269272329b5db7d084447
Armin Eslami <armineslami@hotmail.com>
2024-10-19 21:46:39 +0330

    Update: Increase min width.

--- resources/views/components/toast.blade.php
+++ resources/views/components/toast.blade.php
@@ -1,4 +1,5 @@
-<div {{ $attributes->merge(['class' => 'fixed flex bottom-20 md:bottom-8 mx-auto left-0 right-0 md:right-20 lg:right-0 items-center w-full max-w-xs p-4 space-x-4 rtl:space-x-reverse text-text bg-white dark:bg-gray-700 divide-x rtl:divide-x-reverse divide-gray-300 rounded-lg shadow dark:divide-gray-500']) }} role="alert">
+<div {{ $attributes->merge(['class' => 'fixed flex bottom-20 md:bottom-8 mx-auto left-0 right-0 md:right-20 lg:right-0 items-center w-full max-w-sm p-4 space-x-4 rtl:space-x-reverse text-text bg-white dark:bg-gray-700 divide-x rtl:divide-x-reverse divide-gray-300 rounded-lg shadow dark:divide-gray-500']) }}
+    role="alert">
     {!! $icon !!}
     <div class="ps-4 text-sm font-normal">
         <p class="mt-1">{!! $message !!}</p>
