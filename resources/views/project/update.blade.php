529dc1465c99c6f604a8d4d6d158e8ff5a847b56
Armin Eslami <armineslami@hotmail.com>
2024-10-19 13:40:49 +0330

    Update: Add hidden input with value of formula id.

--- resources/views/project/update.blade.php
+++ resources/views/project/update.blade.php
@@ -24,6 +24,8 @@
             x-bind:action="formAction">
             @csrf
 
+            <input type="hidden" name="formula" value="{{ $project->formula_id }}" autocomplete="off">
+
             <div class="space-y-4 md:space-y-8">
                 <div>
                     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 md:gap-4">
