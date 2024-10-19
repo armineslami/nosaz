e37a64c95417258db1f85c9d62949dc5176ab967
Armin Eslami <armineslami@hotmail.com>
2024-10-19 13:11:13 +0330

    Update: Decrease top margin to create more space.

--- resources/views/layouts/drawer.blade.php
+++ resources/views/layouts/drawer.blade.php
@@ -22,7 +22,7 @@
                 </a>
                 <div>
                     <div aria-controls="project-dropdown" data-collapse-toggle="project-dropdown"
-                        class="flex relative cursor-pointer items-center px-4 py-2 mt-4 rounded-md {{ Route::is('project.*') ? 'primary-button bg-primary text-text-900 dark:text-text-100' : 'primary-button text-gray-900 dark:text-gray-100 hover:bg-primary dark:hover:text-text-100 dark:hover:bg-primary' }}">
+                        class="flex relative cursor-pointer items-center px-4 py-2 mt-2 rounded-md {{ Route::is('project.*') ? 'primary-button bg-primary text-text-900 dark:text-text-100' : 'primary-button text-gray-900 dark:text-gray-100 hover:bg-primary dark:hover:text-text-100 dark:hover:bg-primary' }}">
                         <svg class="size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round"
                                 d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Z" />
@@ -51,7 +51,7 @@
                 </div>
                 <div>
                     <div aria-controls="formula-dropdown" data-collapse-toggle="formula-dropdown"
-                        class="flex cursor-pointer items-center px-4 py-2 mt-4 rounded-md {{ Route::is('formula.*') ? 'primary-button bg-primary text-text-900 dark:text-text-100' : 'primary-button text-gray-900 dark:text-gray-100 hover:bg-primary dark:hover:text-text-100 dark:hover:bg-primary' }}">
+                        class="flex cursor-pointer items-center px-4 py-2 mt-2 rounded-md {{ Route::is('formula.*') ? 'primary-button bg-primary text-text-900 dark:text-text-100' : 'primary-button text-gray-900 dark:text-gray-100 hover:bg-primary dark:hover:text-text-100 dark:hover:bg-primary' }}">
                         <svg class="size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round"
                                 d="M9.75 3.104v5.714a2.25 2.25 0 0 1-.659 1.591L5 14.5M9.75 3.104c-.251.023-.501.05-.75.082m.75-.082a24.301 24.301 0 0 1 4.5 0m0 0v5.714c0 .597.237 1.17.659 1.591L19.8 15.3M14.25 3.104c.251.023.501.05.75.082M19.8 15.3l-1.57.393A9.065 9.065 0 0 1 12 15a9.065 9.065 0 0 0-6.23-.693L5 14.5m14.8.8 1.402 1.402c1.232 1.232.65 3.318-1.067 3.611A48.309 48.309 0 0 1 12 21c-2.773 0-5.491-.235-8.135-.687-1.718-.293-2.3-2.379-1.067-3.61L5 14.5" />
@@ -92,7 +92,7 @@
                         </li>
                     </ul>
                 </div>
-                <a class="flex items-center px-4 py-2 mt-4 rounded-md {{ Route::is('reports') ? 'primary-button flex bg-primary text-text-900 dark:text-text-100' : 'primary-button text-gray-900 dark:text-gray-100 hover:bg-primary dark:hover:text-text-100 dark:hover:bg-primary' }}"
+                <a class="flex items-center px-4 py-2 mt-2 rounded-md {{ Route::is('reports') ? 'primary-button flex bg-primary text-text-900 dark:text-text-100' : 'primary-button text-gray-900 dark:text-gray-100 hover:bg-primary dark:hover:text-text-100 dark:hover:bg-primary' }}"
                     href="{{ route('reports') }}">
                     <svg class="size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round"
