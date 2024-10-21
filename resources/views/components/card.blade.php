18165f178bad58bec03404bb5c57803dbf99e58d
Armin Eslami <armineslami@hotmail.com>
2024-10-21 12:11:42 +0330

    Update: Remove top margin.

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,3 +1,3 @@
-<div {{ $attributes->merge(['type' => 'submit', 'class' => 'bg-background-100 mt-4 md:mt-8 p-4 md:p-8 rounded-lg shadow-sm']) }}>
+<div {{ $attributes->merge(['type' => 'submit', 'class' => 'bg-background-100 p-4 md:p-8 rounded-lg shadow-sm']) }}>
     {{ $slot }}
 </div>
