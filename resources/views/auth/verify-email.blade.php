749ec828e9454190cf6a781b6ea3e16a2a177496
Armin Eslami <armineslami@hotmail.com>
2024-10-21 14:21:59 +0330

    Update: Change order of buttons.

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -2,7 +2,7 @@
     <div class="flex items-center min-h-screen">
         <div class="w-full md:w-1/2 m-4 md:m-auto">
 
-{{--            <x-application-logo class="m-auto w-24 h-24 mb-8 fill-primary" />--}}
+            {{--            <x-application-logo class="m-auto w-24 h-24 mb-8 fill-primary" /> --}}
 
             <div class="p-8 bg-white dark:bg-gray-800 rounded-lg">
                 <div class="mb-8 text-sm text-text text-justify">
@@ -16,6 +16,14 @@
                 @endif
 
                 <div class="mt-4 flex items-center justify-between">
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+
+                        <button type="submit"
+                            class="text-sm text-text-600 dark:text-text-600 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">
+                            {{ __('verify-email.log_out') }}
+                        </button>
+                    </form>
                     <form method="POST" action="{{ route('verification.send') }}">
                         @csrf
 
@@ -24,14 +32,6 @@
                                 {{ __('verify-email.resend_email') }}
                             </x-primary-button>
                         </div>
-                    </form>
-
-                    <form method="POST" action="{{ route('logout') }}">
-                        @csrf
-
-                        <button type="submit" class="text-sm text-text-600 dark:text-text-600 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">
-                            {{ __('verify-email.log_out') }}
-                        </button>
                     </form>
                 </div>
             </div>
