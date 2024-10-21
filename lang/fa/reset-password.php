9ab22ba042e44c67398d44c83d6d673b19dd37bd
Armin Eslami <armineslami@hotmail.com>
2024-10-21 14:16:23 +0330

    Update: Add 'current_password' translation.

--- lang/fa/reset-password.php
+++ lang/fa/reset-password.php
@@ -1,7 +1,8 @@
 <?php
-    return [
-        'email' => 'ایمیل',
-        'password' => 'کلمه عبور',
-        'confirm_password' => 'تایید کلمه عبور',
-        'reset_password' => 'تغییر کلمه عبور'
-    ];
+return [
+    'email' => 'ایمیل',
+    'password' => 'کلمه عبور',
+    'confirm_password' => 'تایید کلمه عبور',
+    'reset_password' => 'تغییر کلمه عبور',
+    'current_password' => 'کلمه عبور فعلی'
+];
