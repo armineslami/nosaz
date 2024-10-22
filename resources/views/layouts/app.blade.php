83d4d9c566da901c4e4a4f246b3bf550bfe49e46
Armin Eslami <armineslami@hotmail.com>
2024-10-22 13:29:27 +0330

    Fix bug: Move app theme configuration to the head html tag to stop the page from appearing light on page load.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,37 +10,6 @@
 
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
-
-<body class="font-sans antialiased">
-    <div class="min-h-screen bg-background">
-
-        @isset($header)
-            <header class="bg-white dark:bg-gray-800 shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
-                </div>
-            </header>
-        @endisset
-
-        @include('layouts.drawer')
-
-        <!-- Page Content -->
-        <div class="relative md:ms-64 h-auto">
-
-            @include('layouts.navigation')
-
-            <main class="p-2 pt-24 pb-20 md:p-4 md:pt-24">
-                {{ $slot }}
-            </main>
-
-            @include('layouts.bottom-navigation')
-        </div>
-
-        <!-- Social modal -->
-        @include('layouts.social-modal')
-
-    </div>
 
     <script>
         const key = 'theme-{{ Auth::user()->email }}';
@@ -87,6 +56,37 @@
             });
         })();
     </script>
+</head>
+
+<body class="font-sans antialiased">
+    <div class="min-h-screen bg-background">
+
+        @isset($header)
+            <header class="bg-white dark:bg-gray-800 shadow">
+                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    {{ $header }}
+                </div>
+            </header>
+        @endisset
+
+        @include('layouts.drawer')
+
+        <!-- Page Content -->
+        <div class="relative md:ms-64 h-auto">
+
+            @include('layouts.navigation')
+
+            <main class="p-2 pt-24 pb-20 md:p-4 md:pt-24">
+                {{ $slot }}
+            </main>
+
+            @include('layouts.bottom-navigation')
+        </div>
+
+        <!-- Social modal -->
+        @include('layouts.social-modal')
+
+    </div>
 </body>
 
 </html>
