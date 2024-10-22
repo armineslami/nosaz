101089cbc4623a3f1c543bf723fb18290b9c4d8d
Armin Eslami <armineslami@hotmail.com>
2024-10-22 09:51:55 +0330

    Update: Provide settings to all views when user is logged in.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,12 @@
 
 namespace App\Providers;
 
+use App\Repositories\SettingsRepository;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -28,6 +31,14 @@ class AppServiceProvider extends ServiceProvider
                 ->line('این ایمیل جهت فعال سازی حساب کاربری شما ارسال شده است. برای انجام این کار کافیست روی دکمه زیر کلیک کنید.')
                 ->action('تایید ایمیل', $url)
                 ->salutation('تیم ' . __('app.name'));
+        });
+
+        // Share the settings with all views
+        View::composer('*', function ($view) {
+            if (Auth::check()) {
+                $settings = SettingsRepository::first();
+                $view->with('settings', $settings);
+            }
         });
     }
 }
