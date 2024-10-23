fd88b30fe131166a023dca02662c4d4debd9d0b6
Armin Eslami <armineslami@hotmail.com>
2024-10-23 09:48:39 +0330

    Add search empty state.

---
+++ resources/views/project/empty-search.blade.php
@@ -0,0 +1,103 @@
+<div class="flex items-center m-auto mt-20 md:mt-32 text-center rounded-lg">
+    <div class="w-full">
+        <div class="relative mb-32">
+            <svg class="absolute top-0 left-0 bottom-0 -right-16 md:right-0 m-auto fill-gray-300 dark:fill-gray-800 opacity-20"
+                id="visual" viewBox="0 0 900 600" width="450" height="300">
+                <g transform="translate(415.80122448615737 318.1032235833984)">
+                    <path id="blob1"
+                        d="M173 -164.4C223 -123 261.5 -61.5 257.5 -4C253.5 53.5 207 107 157 141.2C107 175.3 53.5 190.2 -2.5 192.6C-58.5 195.1 -116.9 185.2 -148.2 151.1C-179.4 116.9 -183.5 58.5 -187.1 -3.7C-190.8 -65.8 -194 -131.5 -162.8 -172.9C-131.5 -214.2 -65.8 -231.1 -2.1 -229C61.5 -226.9 123 -205.7 173 -164.4">
+                    </path>
+                </g>
+                <g class="hidden" transform="translate(454.2142609407367 284.35799449039416)">
+                    <path id="blob2"
+                        d="M145.7 -126.9C195.7 -95.7 247.8 -47.8 247.8 0C247.8 47.8 195.7 95.7 145.7 139.2C95.7 182.7 47.8 221.8 1.8 220.1C-44.3 218.3 -88.6 175.6 -138.3 132.1C-188 88.6 -243 44.3 -254.1 -11.1C-265.1 -66.5 -232.3 -132.9 -182.6 -164.2C-132.9 -195.4 -66.5 -191.5 -9.3 -182.2C47.8 -172.9 95.7 -158.2 145.7 -126.9">
+                    </path>
+                </g>
+            </svg>
+            <svg class="absolute top-0 left-0 bottom-0 -right-5 md:right-0 m-auto fill-gray-300 dark:fill-gray-400 opacity-40"
+                id="visual" viewBox="0 0 900 600" width="300" height="200">
+                <g transform="translate(415.80122448615737 318.1032235833984)">
+                    <path id="blob3"
+                        d="M173 -164.4C223 -123 261.5 -61.5 257.5 -4C253.5 53.5 207 107 157 141.2C107 175.3 53.5 190.2 -2.5 192.6C-58.5 195.1 -116.9 185.2 -148.2 151.1C-179.4 116.9 -183.5 58.5 -187.1 -3.7C-190.8 -65.8 -194 -131.5 -162.8 -172.9C-131.5 -214.2 -65.8 -231.1 -2.1 -229C61.5 -226.9 123 -205.7 173 -164.4">
+                    </path>
+                </g>
+                <g class="hidden" transform="translate(454.2142609407367 284.35799449039416)">
+                    <path id="blob4"
+                        d="M145.7 -126.9C195.7 -95.7 247.8 -47.8 247.8 0C247.8 47.8 195.7 95.7 145.7 139.2C95.7 182.7 47.8 221.8 1.8 220.1C-44.3 218.3 -88.6 175.6 -138.3 132.1C-188 88.6 -243 44.3 -254.1 -11.1C-265.1 -66.5 -232.3 -132.9 -182.6 -164.2C-132.9 -195.4 -66.5 -191.5 -9.3 -182.2C47.8 -172.9 95.7 -158.2 145.7 -126.9">
+                    </path>
+                </g>
+            </svg>
+            <svg class="absolute top-0 left-0 bottom-0 right-0 m-auto fill-gray-300 dark:fill-gray-300 opacity-60"
+                id="visual" viewBox="0 0 900 600" width="200" height="150">
+                <g transform="translate(415.80122448615737 318.1032235833984)">
+                    <path id="blob5"
+                        d="M173 -164.4C223 -123 261.5 -61.5 257.5 -4C253.5 53.5 207 107 157 141.2C107 175.3 53.5 190.2 -2.5 192.6C-58.5 195.1 -116.9 185.2 -148.2 151.1C-179.4 116.9 -183.5 58.5 -187.1 -3.7C-190.8 -65.8 -194 -131.5 -162.8 -172.9C-131.5 -214.2 -65.8 -231.1 -2.1 -229C61.5 -226.9 123 -205.7 173 -164.4">
+                    </path>
+                </g>
+                <g class="hidden" transform="translate(454.2142609407367 284.35799449039416)">
+                    <path id="blob6"
+                        d="M145.7 -126.9C195.7 -95.7 247.8 -47.8 247.8 0C247.8 47.8 195.7 95.7 145.7 139.2C95.7 182.7 47.8 221.8 1.8 220.1C-44.3 218.3 -88.6 175.6 -138.3 132.1C-188 88.6 -243 44.3 -254.1 -11.1C-265.1 -66.5 -232.3 -132.9 -182.6 -164.2C-132.9 -195.4 -66.5 -191.5 -9.3 -182.2C47.8 -172.9 95.7 -158.2 145.7 -126.9">
+                    </path>
+                </g>
+            </svg>
+            <div id="animation-area" class="flex absolute top-0 left-0 bottom-0 right-0 m-auto text-lg size-10">
+                <span id="animation-area-emoji" class="inline-block text-3xl md:text-4xl">
+                    🔍
+                </span>
+            </div>
+        </div>
+
+        <div>
+            <p class="text-text text-sm font-bold mb-2">{{ __('نتیجه‌ای یافت نشد') }}</p>
+            <p class="text-text-600 dark:text-gray-400 text-sm justify-center">
+                {{ __('از کلمه‌ای دیگر برای جستجوی استفاده کنید') }}
+            </p>
+        </div>
+    </div>
+</div>
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        const emoji = document.getElementById('animation-area-emoji');
+
+        setTimeout(() => {
+            loadKute();
+            emoji.classList.add('search-magnifer');
+        }, 100);
+
+        function loadKute() {
+            window.KUTE.fromTo(
+                '#blob1', {
+                    path: '#blob1'
+                }, {
+                    path: '#blob2'
+                }, {
+                    repeat: 999,
+                    duration: 4000,
+                    yoyo: true
+                }
+            ).start(null);
+            window.KUTE.fromTo(
+                '#blob3', {
+                    path: '#blob3'
+                }, {
+                    path: '#blob4'
+                }, {
+                    repeat: 999,
+                    duration: 3500,
+                    yoyo: true
+                }
+            ).start(null);
+            window.KUTE.fromTo(
+                '#blob5', {
+                    path: '#blob5'
+                }, {
+                    path: '#blob6'
+                }, {
+                    repeat: 999,
+                    duration: 3000,
+                    yoyo: true
+                }
+            ).start(null);
+        }
+    });
+</script>
