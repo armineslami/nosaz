eb9e6f12854c6b54f4137ab94969e54c7ce63e32
Armin Eslami <armineslami@hotmail.com>
2024-10-23 17:04:30 +0330

    Update: Improve 'format_number_with_commas' to support truncating numbers.

--- app/Utils/Helpers.php
+++ app/Utils/Helpers.php
@@ -9,14 +9,18 @@ function convert_digits_to_persian($str): string
     return str_replace($latin_chars, $farsi_chars, $str);
 }
 
-function format_number_with_commas($number, $decimals = 0, $decimal_point = '.', $thousands_separator = ',')
+function format_number_with_commas($number, $decimals = 2, $decimal_point = '.', $thousands_separator = ',')
 {
-    // Check if the number is a whole number (e.g., 13.00)
+    // Check if the number is a whole number
     if (floor($number) == $number) {
         // If it's a whole number, format it without decimals
         return number_format($number, 0, $decimal_point, $thousands_separator);
     }
 
-    // Otherwise, format the number with the specified number of decimals
-    return number_format($number, $decimals, $decimal_point, $thousands_separator);
+    // Truncate the number to given number of decimal places without rounding
+    $truncatedNumber = floor($number * pow(10, $decimals)) / pow(10, $decimals);
+
+    // Format the number with the specified number of decimals
+    return number_format($truncatedNumber, $decimals, $decimal_point, $thousands_separator);
 }
+
