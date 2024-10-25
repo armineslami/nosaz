38c2b2db4a4cda299712cfc454d96c8db40b914e
Armin Eslami <armineslami@hotmail.com>
2024-10-25 19:26:26 +0330

    Update: Update texts and formula sample.

--- resources/views/formula/guidance.blade.php
+++ resources/views/formula/guidance.blade.php
@@ -5,11 +5,10 @@
     </p>
 
     <ul class="mt-4 p-4 text-text text-xs space-y-1 list-disc leading-6 lg:leading-5">
-        <li>{{ __('تنها مجاز به استفاده از متغیرها، عملگرهای ریاضی و اعداد هستید. استفاده از هر کاراکتر دیگری منجر به محاسبه اشتباه می‌گردد.') }}
+        <li>
+            {{ __('تنها مجاز به استفاده از متغیرها، برچسب‌ها، عملگرهای ریاضی و اعداد هستید. استفاده از هر کاراکتر دیگری منجر به محاسبه اشتباه می‌گردد.') }}
         </li>
-        <li>{{ __('می‌توانید فرمول خود را در چند خط جدا بنویسید.') }}</li>
-        <li>{{ __('برای اضافه کردن هریک از متغیرها و یا عملگرها کافیست روی آن ضربه بزنید.') }}</li>
-        <li>{{ __('استفاده از دو متغیر یا عملگر پشت سر هم، مجاز نمی‌باشد.') }}</li>
+        <li>{{ __('برای اضافه کردن هریک از متغیرها، برچسب‌ها و یا عملگرها کافیست روی آن ضربه بزنید.') }}</li>
         <li>
             {{ __('زمانی که می‌خواهید نتیجه یک عملیات ریاضی را ذخیره کنید کافیست از ') }}
             <span
@@ -18,9 +17,25 @@
             <span class="text-white bg-accent px-1.5 py-1 rounded-md">{{ __('برچسب') }}</span>
             {{ __(' برای ذخیره سازی استفاده کنید. این برچسب‌ها اطلاعاتی هستند که می‌خواهید محاسبه شده و نمایش داده شوند.') }}
         </li>
+        <li>
+            {{ __('می‌توانید فرمول خود را در چند خط جدا بنویسید. انتهای هر خط با یک ') }}
+            <span
+                class="bg-background-100 border border-primary rounded-md text-text px-3 py-1">{{ '=' }}</span>
+            {{ __(' و به دنبال آن یک ') }}
+            <span class="text-white bg-accent px-1.5 py-1 rounded-md">{{ __('برچسب') }}</span>
+            {{ __('به اتمام می‌رسد.') }}
+        </li>
+        <li>{{ __('استفاده از دو متغیر یا عملگر پشت سر هم، مجاز نمی‌باشد.') }}</li>
         <li>{{ __('در زمان محاسبه پروژه تمامی متغیرها به شما نمایش داده می‌شوند تا مقادیر آن‌ها را وارد کنید و سپس نتیجه محاسبه برای هر یک از برچسب‌ها نمایش داده می‌شود.') }}
         </li>
-        <li>{{ __('به صورت خودکار هر عبارت ریاضی پس از علامت = و برچسب، به پایان می‌رسد.') }}</li>
+        <li>
+            {{ __('به صورت خودکار هر عبارت ریاضی پس از علامت ') }}
+            <span
+                class="bg-background-100 border border-primary rounded-md text-text px-3 py-1">{{ '=' }}</span>
+            {{ __('و ') }}
+            <span class="text-white bg-accent px-1.5 py-1 rounded-md">{{ __('برچسب') }}</span>
+            {{ __('به اتمام می‌رسد.') }}
+        </li>
         <li>{{ __('نمونه‌ای از فرمول:') }}</li>
     </ul>
 
@@ -30,7 +45,11 @@
             <div class="shadow-md bg-primary py-1 px-1.5 rounded-md text-text text-xs select-none">
                 {{ __('متراژ زمین') }}
             </div>
-            <p class="text-text"> * 60 ) / 100 = </p>
+            <p class="text-text">*</p>
+            <div class="shadow-md bg-primary py-1 px-1.5 rounded-md text-text text-xs select-none">
+                {{ __('درصد تراکم') }}
+            </div>
+            <p class="text-text"> ) / 100 = </p>
             <div class="shadow-md bg-accent py-1 px-1.5 rounded-md text-white text-xs select-none">
                 {{ __('متراژ هر سقف') }}
             </div>
@@ -49,6 +68,7 @@
             </div>
         </div>
         <div class="flex items-center gap-1 text-sm mt-2">
+            <p class="text-text">(</p>
             <div class="shadow-md bg-accent py-1 px-1.5 rounded-md text-white text-xs select-none">
                 {{ __('متراژ کل طبقات') }}
             </div>
@@ -56,8 +76,12 @@
             <div class="shadow-md bg-primary py-1 px-1.5 rounded-md text-text text-xs select-none">
                 {{ __('قیمت ساخت هر متر') }}
             </div>
-            <p class="text-text"> = </p>
+            <p class="text-text">) + </p>
             <div class="shadow-md bg-primary py-1 px-1.5 rounded-md text-text text-xs select-none">
+                {{ __('سایر هزینه‌ها') }}
+            </div>
+            <p class="text-text"> = </p>
+            <div class="shadow-md bg-accent py-1 px-1.5 rounded-md text-white text-xs select-none">
                 {{ __('هزینه ساخت') }}
             </div>
         </div>
