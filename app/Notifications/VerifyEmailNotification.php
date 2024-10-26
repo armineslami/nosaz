319fb712a192a56099a84da4d354f7cbb3b927d9
Armin Eslami <armineslami@hotmail.com>
2024-10-26 13:43:42 +0330

    Update: Add.

---
+++ app/Notifications/VerifyEmailNotification.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Auth\Notifications\VerifyEmail;
+
+class VerifyEmailNotification extends VerifyEmail
+{
+    use Queueable;
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail($notifiable): MailMessage
+    {
+        $url = $this->verificationUrl($notifiable);
+
+        return (new MailMessage)
+            ->subject('فعال سازی حساب کاری در ' . __('app.name'))
+            ->greeting('سلام')
+            ->line('این ایمیل جهت فعال سازی حساب کاربری شما ارسال شده است. برای انجام این کار کافیست روی دکمه زیر کلیک کنید.')
+            ->action('تایید ایمیل', $url)
+            ->salutation('تیم ' . __('app.name'));
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,15 +25,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        VerifyEmail::toMailUsing(function (object $notifiable, string $url) {
-            return (new MailMessage)
-                ->subject('فعال سازی حساب کاری در ' . __('app.name'))
-                ->greeting('سلام')
-                ->line('این ایمیل جهت فعال سازی حساب کاربری شما ارسال شده است. برای انجام این کار کافیست روی دکمه زیر کلیک کنید.')
-                ->action('تایید ایمیل', $url)
-                ->salutation('تیم ' . __('app.name'));
-        });
-
         // Share the settings with all views
         View::composer('*', function ($view) {
             if (Auth::check()) {
