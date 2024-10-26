037400880e7c8a48eb919d17ad622b465ab98436
Armin Eslami <armineslami@hotmail.com>
2024-10-26 12:37:36 +0330

    Update: Check for system theme color.

--- resources/views/layouts/basic.blade.php
+++ resources/views/layouts/basic.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
-<html class="dark:dark" dir="rtl"  lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html class="dark:dark" dir="rtl" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -9,10 +10,29 @@
 
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    <script>
+        const systemDarkModeListener = window.matchMedia('(prefers-color-scheme: dark)');
+        systemDarkModeListener.addEventListener('change', function(e) {
+            toggleTheme();
+        });
+
+        toggleTheme();
+
+        function toggleTheme() {
+            if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+                document.documentElement.classList.add('dark');
+            } else {
+                document.documentElement.classList.remove('dark');
+            }
+        }
+    </script>
 </head>
+
 <body class="text-text-950 dark:text-text-50 antialiased bg-background">
-<div>
-    {{ $slot }}
-</div>
+    <div>
+        {{ $slot }}
+    </div>
 </body>
+
 </html>
