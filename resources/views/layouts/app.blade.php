df140d0d5633ca6234a72f096938accac854e9e1
Armin Eslami <armineslami@hotmail.com>
2024-10-27 20:35:20 +0330

    [Update] Use 'head' component to add meta tags and all splash screen images to the page.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,15 +2,6 @@
 <html class="dark:dark" dir="rtl" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', '') }}</title>
-
-    <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-
     <script>
         const key = 'theme-{{ Auth::user()->email }}';
 
@@ -56,6 +47,11 @@
             });
         })();
     </script>
+
+    @include('layouts.head')
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body class="font-sans antialiased">
--- resources/views/layouts/basic.blade.php
+++ resources/views/layouts/basic.blade.php
@@ -2,15 +2,6 @@
 <html class="dark:dark" dir="rtl" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-
     <script>
         const systemDarkModeListener = window.matchMedia('(prefers-color-scheme: dark)');
         systemDarkModeListener.addEventListener('change', function(e) {
@@ -27,6 +18,11 @@
             }
         }
     </script>
+
+    @include('layouts.head')
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body class="text-text-950 dark:text-text-50 antialiased bg-background">
