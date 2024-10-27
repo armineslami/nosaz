b309204efead19fc7e2d679578c800112c219c6c
Armin Eslami <armineslami@hotmail.com>
2024-10-27 13:26:31 +0330

    Update: Reduce the number of kute animation from 3 to 1 to reduce screen freeze issue.

--- resources/views/project/empty.blade.php
+++ resources/views/project/empty.blade.php
@@ -1,8 +1,8 @@
 <div class="flex items-center m-auto mt-20 md:mt-32 text-center rounded-lg">
     <div class="w-full">
         <div class="relative mb-32">
-            <svg class="absolute top-0 left-0 bottom-0 -right-16 md:right-0 m-auto fill-gray-300 dark:fill-gray-800 opacity-20"
-                id="visual" viewBox="0 0 900 600" width="450" height="300">
+            <svg class="w-[300px] h-[250px] md:w-[450px] md:h-[300px] absolute top-0 left-0 bottom-0 right-0 m-auto fill-gray-300 dark:fill-gray-800 opacity-20"
+                id="visual" viewBox="0 0 900 600">
                 <g transform="translate(415.80122448615737 318.1032235833984)">
                     <path id="blob1"
                         d="M173 -164.4C223 -123 261.5 -61.5 257.5 -4C253.5 53.5 207 107 157 141.2C107 175.3 53.5 190.2 -2.5 192.6C-58.5 195.1 -116.9 185.2 -148.2 151.1C-179.4 116.9 -183.5 58.5 -187.1 -3.7C-190.8 -65.8 -194 -131.5 -162.8 -172.9C-131.5 -214.2 -65.8 -231.1 -2.1 -229C61.5 -226.9 123 -205.7 173 -164.4">
@@ -14,8 +14,8 @@
                     </path>
                 </g>
             </svg>
-            <svg class="absolute top-0 left-0 bottom-0 -right-5 md:right-0 m-auto fill-gray-300 dark:fill-gray-400 opacity-40"
-                id="visual" viewBox="0 0 900 600" width="300" height="200">
+            <svg class="w-[280px] h-[150px] md:w-[300px] md:h-[200px] absolute top-0 left-0 bottom-0 right-0 m-auto fill-gray-300 dark:fill-gray-400 opacity-40"
+                id="visual" viewBox="0 0 900 600">
                 <g transform="translate(415.80122448615737 318.1032235833984)">
                     <path id="blob3"
                         d="M173 -164.4C223 -123 261.5 -61.5 257.5 -4C253.5 53.5 207 107 157 141.2C107 175.3 53.5 190.2 -2.5 192.6C-58.5 195.1 -116.9 185.2 -148.2 151.1C-179.4 116.9 -183.5 58.5 -187.1 -3.7C-190.8 -65.8 -194 -131.5 -162.8 -172.9C-131.5 -214.2 -65.8 -231.1 -2.1 -229C61.5 -226.9 123 -205.7 173 -164.4">
@@ -27,8 +27,8 @@
                     </path>
                 </g>
             </svg>
-            <svg class="absolute top-0 left-0 bottom-0 right-0 m-auto fill-gray-300 dark:fill-gray-300 opacity-60"
-                id="visual" viewBox="0 0 900 600" width="200" height="150">
+            <svg class="w-[250px] h-[100px] md:w-[200px] md:h-[150px] absolute top-0 left-0 bottom-0 right-0 m-auto fill-gray-300 dark:fill-gray-300 opacity-60"
+                id="visual" viewBox="0 0 900 600">
                 <g transform="translate(415.80122448615737 318.1032235833984)">
                     <path id="blob5"
                         d="M173 -164.4C223 -123 261.5 -61.5 257.5 -4C253.5 53.5 207 107 157 141.2C107 175.3 53.5 190.2 -2.5 192.6C-58.5 195.1 -116.9 185.2 -148.2 151.1C-179.4 116.9 -183.5 58.5 -187.1 -3.7C-190.8 -65.8 -194 -131.5 -162.8 -172.9C-131.5 -214.2 -65.8 -231.1 -2.1 -229C61.5 -226.9 123 -205.7 173 -164.4">
@@ -92,42 +92,22 @@
             }, delay); // Total time of animation (slide-in, pause, and slide-out)
         }
 
-        setTimeout(loadKute, 0);
+        setTimeout(() => {
+            loadKute("#blob1", "#blob1", "#blob2", 999, 4000, true);
+            // loadKute("#blob3", "#blob3", "#blob4", 999, 3500, true);
+            // loadKute("#blob5", "#blob5", "#blob6", 999, 3000, true);
+        }, 150);
 
-        function loadKute() {
-            window.KUTE.fromTo(
-                '#blob1', {
-                    path: '#blob1'
-                }, {
-                    path: '#blob2'
-                }, {
-                    repeat: 999,
-                    duration: 4000,
-                    yoyo: true
-                }
-            ).start(null);
-            window.KUTE.fromTo(
-                '#blob3', {
-                    path: '#blob3'
-                }, {
-                    path: '#blob4'
-                }, {
-                    repeat: 999,
-                    duration: 3500,
-                    yoyo: true
-                }
-            ).start(null);
-            window.KUTE.fromTo(
-                '#blob5', {
-                    path: '#blob5'
-                }, {
-                    path: '#blob6'
-                }, {
-                    repeat: 999,
-                    duration: 3000,
-                    yoyo: true
-                }
-            ).start(null);
+        function loadKute(element, from, to, repeat, duration, yoyo) {
+            window.KUTE.fromTo(element, {
+                path: from
+            }, {
+                path: to
+            }, {
+                repeat,
+                duration,
+                yoyo
+            }).start(null);
         }
     });
 </script>
