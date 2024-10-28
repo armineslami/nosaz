69f29a400bf3346ed9d324774087d83e1ae28cf4
Armin Eslami <armineslami@hotmail.com>
2024-10-28 17:17:14 +0330

    [Update] Add logo animation.

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -34,12 +34,35 @@
             <div class="col-span-1 lg:col-span-6 bg-primary hidden lg:flex items-center">
                 <div class="text-justify p-8 w-full">
                     <div class="flex justify-center">
-                        <a href="/">
-                            <x-logo width="192px" height="192px" primaryColor="#5F5F5F" secondaryColor="#ffffff"
-                                background="" />
-                        </a>
+                        <div id="guest-logo" class='w-[fit-content] px-2 py-2 rounded-xl mb-8'>
+                            <svg class="pt-24 -mt-24" width="216px" height="216px" viewBox="0 0 428.339869 272.758591"
+                                version="1.1" xmlns="http://www.w3.org/2000/svg"
+                                xmlns:xlink="http://www.w3.org/1999/xlink">
+                                <g id="Main" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                    <g id="Icons" transform="translate(-2453.644046, -119.977711)">
+                                        <g id="Black-Inner-White" transform="translate(2412.000000, 0.000000)">
+                                            <g id="Group" transform="translate(41.644046, 119.977711)">
+                                                <rect id="home" fill="#ffffff" x="93.4081393" y="83.5559381"
+                                                    width="122.686095" height="189.202653" rx="12"></rect>
+                                                <path
+                                                    d="M0,260.990242 L54.5248811,260.990242 C56.7340201,260.990242 58.5248811,259.199381 58.5248811,256.990242 L58.5248811,171.046886 C58.5248811,169.494238 59.4233813,168.081854 60.8297058,167.423852 L249.335681,79.2241232 C250.742006,78.5661205 251.640506,77.1537366 251.640506,75.6010885 L251.640506,4 C251.640506,1.790861 249.849645,-2.47250396e-15 247.640506,-1.42108547e-14 L166.64141,-1.42108547e-14 C164.432271,1.44774501e-15 162.64141,1.790861 162.64141,4 L162.64141,227.265209 C162.64141,229.474348 164.432271,231.265209 166.64141,231.265209 L340.244026,231.265209 C342.453165,231.265209 344.244026,229.474348 344.244026,227.265209 L344.244026,154.307615 C344.244026,152.928237 343.533324,151.646187 342.363503,150.915297 L259.380973,99.0688691 C257.507448,97.8983144 255.039733,98.4681846 253.869179,100.34171 C253.47942,100.965536 253.269225,101.684756 253.261705,102.420293 L251.681819,256.949349 C251.659235,259.158372 253.431693,260.967448 255.640717,260.990033 C255.654348,260.990172 255.667979,260.990242 255.68161,260.990242 L428.339869,260.990242 L428.339869,260.990242"
+                                                    id="homes" stroke="#5F5F5F" stroke-width="24"></path>
+                                                <ellipse id="moon" fill="#ffffff" cx="313.695561" cy="53.9916994"
+                                                    rx="41.3880802" ry="40.6490074"></ellipse>
+                                                <g id="sun" class="hidden">
+                                                    <text font-family="AppleColorEmoji, Apple Color Emoji"
+                                                        font-size="92" font-weight="normal" fill="#000000">
+                                                        <tspan x="380.355954" y="92">☀️</tspan>
+                                                    </text>
+                                                </g>
+                                            </g>
+                                        </g>
+                                    </g>
+                                </g>
+                            </svg>
+                        </div>
                     </div>
-                    <div class="mt-8 text-center">
+                    <div class="mt-0 text-center">
                         <h1 class="font-bold text-4xl text-text dark:text-text-50 leading-relaxed">
                             {{ __('محاسبات پروژه‌های خود را آسان کنید.') }}
                         </h1>
@@ -120,6 +143,41 @@
                 }, 200);
             }
         }
+
+        document.addEventListener("DOMContentLoaded", () => {
+            const guestlogo = document.getElementById("guest-logo");
+            const guestMoon = guestlogo.querySelector("svg").getElementById("moon");
+            const guestSun = guestlogo.querySelector("svg").getElementById("sun");
+
+            setTimeout(() => {
+                guestDay();
+            }, 3000);
+
+            function guestDay() {
+                if (!runAnimation) return;
+                guestMoon.classList.remove("move-down-animation");
+                guestMoon.classList.add("move-up-animation");
+                setTimeout(() => {
+                    guestSun.classList.add("move-down-animation");
+                    guestSun.classList.remove("hidden");
+                    setTimeout(() => {
+                        guestNight();
+                    }, 5000)
+                }, 200);
+            }
+
+            function guestNight() {
+                guestSun.classList.remove("move-down-animation");
+                guestSun.classList.add("move-up-animation");
+                setTimeout(() => {
+                    guestMoon.classList.remove("move-up-animation");
+                    guestMoon.classList.add("move-down-animation");
+                    setTimeout(() => {
+                        guestDay();
+                    }, 5000)
+                }, 200);
+            }
+        })
     </script>
 </body>
 
