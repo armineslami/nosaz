4f6baac4072dae322dfc5280aa27ebea735ea022
Armin Eslami <armineslami@hotmail.com>
2024-10-30 15:18:44 +0330

    [Update] Set email template to use app colors.

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,12 +1,16 @@
 @props(['url'])
 <tr>
-<td class="header">
-<a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
-<img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
-@else
-{{ $slot }}
-@endif
-</a>
-</td>
+    <td class="header" style="background:#1D1E1F;">
+        {{-- <div class="bg-[#1D1E1F] rounded-lg" style="width: 48px; height:48px;">
+            <x-logo width="48px" height="48px" />
+        </div> --}}
+        <x-logo width="64px" height="64px" />
+        <a href="{{ $url }}" style="display: inline-block;">
+            @if (trim($slot) === 'Laravel')
+                <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
+            @else
+                {{ $slot }}
+            @endif
+        </a>
+    </td>
 </tr>
