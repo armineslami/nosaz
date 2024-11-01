a0458cdc82dc58bfe2afa9fc8be18d282b32e63b
Armin Eslami <armineslami@hotmail.com>
2024-11-01 11:31:33 +0330

    [Update] Reduce text size of header for small screen devices.

--- resources/views/auth/privacy-policy.blade.php
+++ resources/views/auth/privacy-policy.blade.php
@@ -1,62 +1,87 @@
 <x-basic-layout>
     <div class="bg-primary p-16 text-center">
-        <p class="text-text text-4xl font-bold">{{ __('privacy.privacy_policy') }}</p>
+        <p class="text-text text-2xl md:text-4xl font-bold">{{ __('privacy.privacy_policy') }}</p>
     </div>
 
     <div class="flex items-center w-full">
         <div class="w-full p-8">
             <div class="mt-8">
-                <p class="text-text text-base font-normal text-justify">{{ __('privacy.privacy_policy_description') }}</p>
+                <p class="text-text text-base font-normal text-justify">{{ __('privacy.privacy_policy_description') }}
+                </p>
             </div>
 
             <div class="mt-8">
                 <p class="text-text text-lg font-bold mb-8">{{ __('privacy.what_information_do_we_collect') }}</p>
 
-                <p class="text-text text-base font-semibold text-justify mt-4">{{ __('privacy.personal_information_you_disclose_to_us') }}</p>
-                <p class="text-text text-base font-normal text-justify mt-4">{{ __('privacy.personal_information_you_disclose_to_us_description') }}</p>
+                <p class="text-text text-base font-semibold text-justify mt-4">
+                    {{ __('privacy.personal_information_you_disclose_to_us') }}</p>
+                <p class="text-text text-base font-normal text-justify mt-4">
+                    {{ __('privacy.personal_information_you_disclose_to_us_description') }}</p>
 
-                <p class="text-text text-base font-semibold text-justify mt-4">{{ __('privacy.information_automatically_collected') }}</p>
-                <p class="text-text text-base font-normal text-justify mt-4">{{ __('privacy.information_automatically_collected_description') }}</p>
+                <p class="text-text text-base font-semibold text-justify mt-4">
+                    {{ __('privacy.information_automatically_collected') }}</p>
+                <p class="text-text text-base font-normal text-justify mt-4">
+                    {{ __('privacy.information_automatically_collected_description') }}</p>
 
                 <p class="text-text text-lg font-bold my-8">{{ __('privacy.how_do_we_process_your_information') }}</p>
-                <p class="text-text text-base font-normal text-justify mt-4">{{ __('privacy.how_do_we_process_your_information_description1') }}</p>
-                <p class="text-text text-base font-normal text-justify mt-4">{{ __('privacy.how_do_we_process_your_information_description2') }}</p>
+                <p class="text-text text-base font-normal text-justify mt-4">
+                    {{ __('privacy.how_do_we_process_your_information_description1') }}</p>
+                <p class="text-text text-base font-normal text-justify mt-4">
+                    {{ __('privacy.how_do_we_process_your_information_description2') }}</p>
 
-                <p class="text-text text-lg font-bold my-8">{{ __('privacy.when_and_with_whom_do_we_share_your_personal_information') }}</p>
-                <p class="text-text text-base font-normal text-justify mt-4">{{ __('privacy.when_and_with_whom_do_we_share_your_personal_information_description') }}</p>
+                <p class="text-text text-lg font-bold my-8">
+                    {{ __('privacy.when_and_with_whom_do_we_share_your_personal_information') }}</p>
+                <p class="text-text text-base font-normal text-justify mt-4">
+                    {{ __('privacy.when_and_with_whom_do_we_share_your_personal_information_description') }}</p>
 
-                <p class="text-text text-base font-semibold text-justify mt-4">{{ __('privacy.payment_processing_partners') }}</p>
-                <p class="text-text text-base font-normal text-justify mt-4">{{ __('privacy.payment_processing_partners_description') }}</p>
+                <p class="text-text text-base font-semibold text-justify mt-4">
+                    {{ __('privacy.payment_processing_partners') }}</p>
+                <p class="text-text text-base font-normal text-justify mt-4">
+                    {{ __('privacy.payment_processing_partners_description') }}</p>
 
                 <p class="text-text text-base font-semibold text-justify mt-4">{{ __('privacy.legal_compliance') }}</p>
-                <p class="text-text text-base font-normal text-justify mt-4">{{ __('privacy.legal_compliance_description') }}</p>
+                <p class="text-text text-base font-normal text-justify mt-4">
+                    {{ __('privacy.legal_compliance_description') }}</p>
 
                 <p class="text-text text-base font-semibold text-justify mt-4">{{ __('privacy.your_consent') }}</p>
-                <p class="text-text text-base font-normal text-justify mt-4">{{ __('privacy.your_consent_description') }}</p>
+                <p class="text-text text-base font-normal text-justify mt-4">
+                    {{ __('privacy.your_consent_description') }}</p>
 
-                <p class="text-text text-base font-semibold text-justify mt-4">{{ __('privacy.business_transfers') }}</p>
-                <p class="text-text text-base font-normal text-justify mt-4">{{ __('privacy.business_transfers_description') }}</p>
+                <p class="text-text text-base font-semibold text-justify mt-4">{{ __('privacy.business_transfers') }}
+                </p>
+                <p class="text-text text-base font-normal text-justify mt-4">
+                    {{ __('privacy.business_transfers_description') }}</p>
 
-                <p class="text-text text-base font-semibold text-justify mt-4">{{ __('privacy.protecting_our_rights') }}</p>
-                <p class="text-text text-base font-normal text-justify mt-4">{{ __('privacy.protecting_our_rights_description') }}</p>
+                <p class="text-text text-base font-semibold text-justify mt-4">
+                    {{ __('privacy.protecting_our_rights') }}</p>
+                <p class="text-text text-base font-normal text-justify mt-4">
+                    {{ __('privacy.protecting_our_rights_description') }}</p>
 
                 <p class="text-text text-lg font-semibold text-justify mt-4">{{ __('privacy.do_we_use_cookies') }}</p>
-                <p class="text-text text-base font-normal text-justify mt-4">{{ __('privacy.do_we_use_cookies_description') }}</p>
+                <p class="text-text text-base font-normal text-justify mt-4">
+                    {{ __('privacy.do_we_use_cookies_description') }}</p>
 
-                <p class="text-text text-lg font-semibold text-justify mt-4">{{ __('privacy.how_do_we_handle_your_social_logins') }}</p>
-                <p class="text-text text-base font-normal text-justify mt-4">{{ __('privacy.how_do_we_handle_your_social_logins_description') }}</p>
+                <p class="text-text text-lg font-semibold text-justify mt-4">
+                    {{ __('privacy.how_do_we_handle_your_social_logins') }}</p>
+                <p class="text-text text-base font-normal text-justify mt-4">
+                    {{ __('privacy.how_do_we_handle_your_social_logins_description') }}</p>
 
-                <p class="text-text text-lg font-semibold text-justify mt-4">{{ __('privacy.how_long_do_we_keep_your_information') }}</p>
-                <p class="text-text text-base font-normal text-justify mt-4">{{ __('privacy.how_long_do_we_keep_your_information_description') }}</p>
+                <p class="text-text text-lg font-semibold text-justify mt-4">
+                    {{ __('privacy.how_long_do_we_keep_your_information') }}</p>
+                <p class="text-text text-base font-normal text-justify mt-4">
+                    {{ __('privacy.how_long_do_we_keep_your_information_description') }}</p>
 
-                <p class="text-text text-lg font-semibold text-justify mt-4">{{ __('privacy.do_we_make_updates_to_this_notice') }}</p>
-                <p class="text-text text-base font-normal text-justify mt-4">{{ __('privacy.do_we_make_updates_to_this_notice_description') }}</p>
+                <p class="text-text text-lg font-semibold text-justify mt-4">
+                    {{ __('privacy.do_we_make_updates_to_this_notice') }}</p>
+                <p class="text-text text-base font-normal text-justify mt-4">
+                    {{ __('privacy.do_we_make_updates_to_this_notice_description') }}</p>
 
-                <p class="text-text text-lg font-semibold text-justify mt-4">{{ __('privacy.how_can_you_contact_us_about_this_notice') }}</p>
+                <p class="text-text text-lg font-semibold text-justify mt-4">
+                    {{ __('privacy.how_can_you_contact_us_about_this_notice') }}</p>
                 <p class="text-text text-base font-normal text-justify mt-4">
                     {{ __('privacy.how_can_you_contact_us_about_this_notice_description') }}
                     <span>{{ config('app.support_email') }}</span>
                 </p>
+            </div>
         </div>
-    </div>
 </x-basic-layout>
--- resources/views/auth/terms-of-service.blade.php
+++ resources/views/auth/terms-of-service.blade.php
@@ -1,6 +1,6 @@
 <x-basic-layout>
     <div class="bg-primary p-16 text-center">
-        <p class="text-text text-4xl font-bold">{{ __('terms.terms_of_service') }}</p>
+        <p class="text-text text-2xl md:text-4xl font-bold">{{ __('terms.terms_of_service') }}</p>
     </div>
 
     <div class="flex items-center w-full">
@@ -17,60 +17,77 @@
 
             <div class="mt-8">
                 <p class="text-text text-lg font-bold mb-4">{{ __('terms.your_submission') }}</p>
-                <p class="text-text text-base font-normal text-justify">{{ __('terms.your_submission_description1') }}</p>
-                <p class="text-text text-base font-normal text-justify mt-6">{{ __('terms.your_responsible_for_what_you_upload') }}</p>
-                <p class="text-text text-base font-normal text-justify mt-4">{{ __('terms.your_submission_description2') }}</p>
+                <p class="text-text text-base font-normal text-justify">{{ __('terms.your_submission_description1') }}
+                </p>
+                <p class="text-text text-base font-normal text-justify mt-6">
+                    {{ __('terms.your_responsible_for_what_you_upload') }}</p>
+                <p class="text-text text-base font-normal text-justify mt-4">
+                    {{ __('terms.your_submission_description2') }}</p>
             </div>
 
             <div class="mt-8">
                 <p class="text-text text-lg font-bold mb-4">{{ __('terms.user_registration') }}</p>
-                <p class="text-text text-base font-normal text-justify">{{ __('terms.user_registration_description') }}</p>
+                <p class="text-text text-base font-normal text-justify">{{ __('terms.user_registration_description') }}
+                </p>
             </div>
 
             <div class="mt-8">
                 <p class="text-text text-lg font-bold mb-8">{{ __('terms.purchase_and_payment') }}</p>
 
                 <p class="text-text text-base font-semibold text-justify mt-4">{{ __('terms.payment_processing') }}</p>
-                <p class="text-text text-base font-normal text-justify mt-4">{{ __('terms.payment_processing_description') }}</p>
+                <p class="text-text text-base font-normal text-justify mt-4">
+                    {{ __('terms.payment_processing_description') }}</p>
 
-                <p class="text-text text-base font-semibold text-justify mt-4">{{ __('terms.payment_methods_and_currency') }}</p>
-                <p class="text-text text-base font-normal text-justify mt-4">{{ __('terms.payment_methods_and_currency_description') }}</p>
+                <p class="text-text text-base font-semibold text-justify mt-4">
+                    {{ __('terms.payment_methods_and_currency') }}</p>
+                <p class="text-text text-base font-normal text-justify mt-4">
+                    {{ __('terms.payment_methods_and_currency_description') }}</p>
 
                 <p class="text-text text-base font-semibold text-justify mt-4">{{ __('terms.security') }}</p>
                 <p class="text-text text-base font-normal text-justify mt-4">{{ __('terms.security_description') }}</p>
 
-                <p class="text-text text-base font-semibold text-justify mt-4">{{ __('terms.subscription_services') }}</p>
-                <p class="text-text text-base font-normal text-justify mt-4">{{ __('terms.subscription_services_description') }}</p>
+                <p class="text-text text-base font-semibold text-justify mt-4">{{ __('terms.subscription_services') }}
+                </p>
+                <p class="text-text text-base font-normal text-justify mt-4">
+                    {{ __('terms.subscription_services_description') }}</p>
 
                 <p class="text-text text-base font-semibold text-justify mt-4">{{ __('terms.changes_in_fees') }}</p>
-                <p class="text-text text-base font-normal text-justify mt-4">{{ __('terms.changes_in_fees_description') }}</p>
+                <p class="text-text text-base font-normal text-justify mt-4">
+                    {{ __('terms.changes_in_fees_description') }}</p>
 
                 <p class="text-text text-base font-semibold text-justify mt-4">{{ __('terms.responsibility') }}</p>
-                <p class="text-text text-base font-normal text-justify mt-4">{{ __('terms.responsibility_description') }}</p>
+                <p class="text-text text-base font-normal text-justify mt-4">
+                    {{ __('terms.responsibility_description') }}</p>
 
                 <p class="text-text text-base font-semibold text-justify mt-4">{{ __('terms.taxes') }}</p>
                 <p class="text-text text-base font-normal text-justify mt-4">{{ __('terms.taxes_description') }}</p>
 
                 <p class="text-text text-base font-semibold text-justify mt-4">{{ __('terms.prohibited_use') }}</p>
-                <p class="text-text text-base font-normal text-justify mt-4">{{ __('terms.prohibited_use_description') }}</p>
+                <p class="text-text text-base font-normal text-justify mt-4">
+                    {{ __('terms.prohibited_use_description') }}</p>
             </div>
 
             <div class="mt-8">
                 <p class="text-text text-lg font-bold mb-4">{{ __('terms.prohibited_activities') }}</p>
-                <p class="text-text text-base font-normal text-justify">{{ __('terms.prohibited_activities_description1') }}</p>
+                <p class="text-text text-base font-normal text-justify">
+                    {{ __('terms.prohibited_activities_description1') }}</p>
 
-                <p class="text-text text-base font-normal text-justify mt-4">{{ __('terms.as_a_user_of_the_service_you_agree_not_to') }}</p>
-                <p class="text-text text-base font-normal text-justify mt-4">{{ __('terms.prohibited_activities_description2') }}</p>
+                <p class="text-text text-base font-normal text-justify mt-4">
+                    {{ __('terms.as_a_user_of_the_service_you_agree_not_to') }}</p>
+                <p class="text-text text-base font-normal text-justify mt-4">
+                    {{ __('terms.prohibited_activities_description2') }}</p>
             </div>
 
             <div class="mt-8">
                 <p class="text-text text-lg font-bold mb-4">{{ __('terms.privacy_policy') }}</p>
-                <p class="text-text text-base font-normal text-justify">{{ __('terms.privacy_policy_description') }}</p>
+                <p class="text-text text-base font-normal text-justify">{{ __('terms.privacy_policy_description') }}
+                </p>
             </div>
 
             <div class="mt-8">
                 <p class="text-text text-lg font-bold mb-4">{{ __('terms.modifications_and_interruptions') }}</p>
-                <p class="text-text text-base font-normal text-justify">{{ __('terms.modifications_and_interruptions_description') }}</p>
+                <p class="text-text text-base font-normal text-justify">
+                    {{ __('terms.modifications_and_interruptions_description') }}</p>
             </div>
 
             <div class="mt-8">
