52c9db90fb9f63bcebbaca2c028244457580ac08
Armin Eslami <armineslami@hotmail.com>
2024-11-01 11:29:39 +0330

    [Update] Add back button for small screen devices.

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -5,7 +5,17 @@
             {{-- <x-application-logo class="m-auto w-24 h-24 mb-8 rounded-lg px-2 bg-primary" /> --}}
 
             <div>
-                <p class="text-text text-lg font-bold mb-2">{{ __('تغییر کلمه عبور') }}</p>
+                <div class="flex">
+                    <a href="{{ route('login') }}"
+                        class="flex md:hidden h-[fit-content] me-2  text-sm p-2 relative bottom-2 text-gray-500 rounded-lg hover:text-text-900 hover:bg-primary dark:text-gray-400 dark:hover:text-text-100 dark:hover:bg-primary">
+                        <span class="sr-only">{{ __('بازگشت') }}</span>
+                        <svg class="size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
+                        </svg>
+                    </a>
+                    <p class="text-text text-lg font-bold mb-2">{{ __('تغییر کلمه عبور') }}</p>
+                </div>
                 <p class="text-text-600 dark:text-gray-400 text-sm justify-center">
                     {{ __('از یک کلمه عبور طولانی و غیر قابل حدس استفاده کنید.') }}
                 </p>
@@ -44,9 +54,9 @@
                         <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
                     </div>
 
-                    <div class="flex items-center justify-between mt-8">
+                    <div class="flex items-center justify-end md:justify-between mt-8">
                         <a href="{{ route('login') }}"
-                            class="text-sm text-text-600 dark:text-text-600 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800 cursor-pointer">
+                            class="hidden md:block text-sm text-text-600 dark:text-text-600 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800 cursor-pointer">
                             {{ __('بازگشت') }}
                         </a>
                         <x-primary-button>
