3fe9deb6e1e0244bdf1aa083ba2d39ea4d333168
Armin Eslami <armineslami@hotmail.com>
2024-11-03 14:18:19 +0330

    [Update] Remove log.

--- resources/js/sw.js
+++ resources/js/sw.js
@@ -7,12 +7,14 @@ if ("serviceWorker" in navigator) {
             // Check if there's an existing service worker controlling the page
             const isFirstInstall = !navigator.serviceWorker.controller;
 
-            console.log("Is this new installation: ", isFirstInstall);
-
             reg.onupdatefound = () => {
                 const newWorker = reg.installing;
                 newWorker.onstatechange = () => {
-                    // Check if the new service worker has been activated and is controlling the page
+                    /**
+                     * Check if the new service worker has been activated and is controlling the page.
+                     * If there is a activated service controller and this is not the first install,
+                     * show a prompt to the user to inform that an app update is installed.
+                     */
                     if (newWorker.state === "activated" && !isFirstInstall) {
                         // Display a message to let the user know about the update
                         const event = new CustomEvent("open-modal", {
