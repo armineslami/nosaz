478236c77ba22bc1d584575fee3fb1eb438161ca
Armin Eslami <armineslami@hotmail.com>
2024-11-03 12:51:46 +0330

    [Update] Only show the update modal if it's not the initial install of service worker.

--- resources/js/sw.js
+++ resources/js/sw.js
@@ -3,13 +3,17 @@ if ("serviceWorker" in navigator) {
         .register("/service-worker.js")
         .then((reg) => {
             // console.log("Service Worker registered!", reg);
+
+            // Check if there's an existing service worker controlling the page
+            const isFirstInstall = !navigator.serviceWorker.controller;
+
+            console.log("Is this new installation: ", isFirstInstall);
+
             reg.onupdatefound = () => {
                 const newWorker = reg.installing;
                 newWorker.onstatechange = () => {
-                    if (
-                        newWorker.state === "activated" &&
-                        navigator.serviceWorker.controller
-                    ) {
+                    // Check if the new service worker has been activated and is controlling the page
+                    if (newWorker.state === "activated" && !isFirstInstall) {
                         // Display a message to let the user know about the update
                         const event = new CustomEvent("open-modal", {
                             detail: "update-app-prompt",
