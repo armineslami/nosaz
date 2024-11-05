4325aca89f1b67a4305bdee5128e513a56b33bb9
Armin Eslami <armineslami@hotmail.com>
2024-11-05 16:42:09 +0330

    Set version to 0.0.1

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const VERSION = "v0.0.5";
+const VERSION = "v0.0.1";
 const CACHE_NAME = `nosaz-app-cache-${VERSION}`;
 const OFFLINE_URL = "/offline";
 const ASSET_MANIFEST_URL = "/build/manifest.json";
