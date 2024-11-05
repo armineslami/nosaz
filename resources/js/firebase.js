995440667e52ec7f2f47d10296f47f56a263a029
Armin Eslami <armineslami@hotmail.com>
2024-11-05 12:09:11 +0330

    [Update] Refactor.

--- resources/js/firebase.js
+++ resources/js/firebase.js
@@ -1,141 +1,117 @@
 import { initializeApp } from "firebase/app";
 import { getMessaging, getToken, onMessage } from "firebase/messaging";
 
-// let messaging = null;
+class Firebase {
+    constructor() {
+        this.messaging = null;
+        this.initializeFirebase();
+    }
 
-// initialize();
+    initializeFirebase() {
+        const firebaseConfig = {
+            apiKey: "AIzaSyDKs9rp7sGQr8TP5yUEI1_YJ3llg8LPZ1k",
+            authDomain: "nosaz-f1a86.firebaseapp.com",
+            projectId: "nosaz-f1a86",
+            storageBucket: "nosaz-f1a86.firebasestorage.app",
+            messagingSenderId: "47873885304",
+            appId: "1:47873885304:web:3a511741e8f1713056998d",
+            measurementId: "G-N8FX7TEKXL",
+        };
 
-// function initialize() {
-//     console.log("==>", "serviceWorker" in navigator);
-//     if (
-//         !("serviceWorker" in navigator) ||
-//         !("Notification" in window) ||
-//         !("PushManager" in window)
-//     ) {
-//         console.log("Browser does not support sending notification");
-//         return;
-//     }
+        //   const firebaseConfig = {
+        //     apiKey: "AIzaSyDKs9rp7sGQr8TP5yUEI1_YJ3llg8LPZ1k",
+        //     authDomain: "nosaz-f1a86.firebaseapp.com",
+        //     projectId: "nosaz-f1a86",
+        //     storageBucket: "nosaz-f1a86.firebasestorage.app",
+        //     messagingSenderId: "47873885304",
+        //     appId: "1:47873885304:web:aceadfde9151848156998d",
+        //     measurementId: "G-9XY26MSEC8"
+        //   };
 
-//     /**
-//      * FCM Initialization
-//      */
+        const app = initializeApp(firebaseConfig);
+        this.messaging = getMessaging(app);
+    }
 
-//     const firebaseConfig = {
-//         apiKey: "AIzaSyByJLWSl0XUpzRZHPQRf9x2fJmO6TXsvWM",
-//         authDomain: "nosaz-8e631.firebaseapp.com",
-//         projectId: "nosaz-8e631",
-//         storageBucket: "nosaz-8e631.firebasestorage.app",
-//         messagingSenderId: "759253213886",
-//         appId: "1:759253213886:web:c5395d715677944b9274ef",
-//         measurementId: "G-EMYZQPB5F9",
-//     };
+    async register() {
+        try {
+            await this.registerServiceWorker();
+            await this.getFCMToken();
+            this.listenForMessages();
+        } catch (error) {
+            console.error("Failed to initialize Firebase:", error);
+        }
+    }
 
-//     // Initialize Firebase
-//     const app = initializeApp(firebaseConfig);
-//     messaging = getMessaging(app);
+    async registerServiceWorker() {
+        try {
+            const registration = await navigator.serviceWorker.register(
+                "/firebase-messaging-sw.js",
+                { scope: "fcm" }
+            );
+            console.log(
+                "Service Worker registered with scope:",
+                registration.scope
+            );
 
-//     registerServiceWorker();
-// }
+            if (!navigator.serviceWorker.controller) {
+                await new Promise((resolve) => {
+                    navigator.serviceWorker.addEventListener(
+                        "controllerchange",
+                        resolve,
+                        { once: true }
+                    );
+                });
+                console.log("Service Worker is now controlling the page.");
+            }
+        } catch (error) {
+            console.error("Service Worker registration failed:", error);
+        }
+    }
 
-/**
- * FCM Initialization
- */
-
-const firebaseConfig = {
-    apiKey: "AIzaSyByJLWSl0XUpzRZHPQRf9x2fJmO6TXsvWM",
-    authDomain: "nosaz-8e631.firebaseapp.com",
-    projectId: "nosaz-8e631",
-    storageBucket: "nosaz-8e631.firebasestorage.app",
-    messagingSenderId: "759253213886",
-    appId: "1:759253213886:web:c5395d715677944b9274ef",
-    measurementId: "G-EMYZQPB5F9",
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const messaging = getMessaging(app);
-
-registerServiceWorker();
-
-// Register the service worker and then get the FCM token
-async function registerServiceWorker() {
-    try {
-        const registration = await navigator.serviceWorker.register(
-            "/firebase-messaging-sw.js",
-            { scope: "fcm" }
-        );
-        console.log(
-            "Service Worker registered with scope:",
-            registration.scope
-        );
-
-        // Wait until the service worker takes control
-        if (!navigator.serviceWorker.controller) {
-            await new Promise((resolve) => {
-                navigator.serviceWorker.addEventListener(
-                    "controllerchange",
-                    resolve,
-                    { once: true }
-                );
+    async getFCMToken(retries = 3) {
+        console.log("Getting FCM token...");
+        try {
+            const token = await getToken(this.messaging, {
+                vapidKey:
+                    "BJWatfCQoF9P7ittgUnbxTa2wzRoql53KijkeMXTTBZAARraG0vo-NXLALoUmoNjK7_0yNggI2oWHdX_Lw11pGA",
             });
-            console.log("Service Worker is now controlling the page.");
-        }
 
-        // Safe to retrieve FCM token after the service worker is active
-        await getFCMToken();
-    } catch (error) {
-        console.error("Service Worker registration failed:", error);
-    }
-}
-
-async function getFCMToken(retries = 3) {
-    try {
-        const token = await getToken(messaging, {
-            vapidKey:
-                "BK86acGo2FOs2DBFwH4dC8YDg3TT0XbnLty0_alSPjcrUk0AjjRZGTomqbsoVEdMbB6Mlyy0QE6fsltBUdGuuQM",
-        });
-
-        if (token) {
-            console.log("FCM Token:", token);
-            // Send token to server if necessary
-        } else {
-            console.log(
-                "No registration token available. Requesting permission."
-            );
-            await requestNotificationPermission();
-        }
-    } catch (error) {
-        if (retries > 0 && error.name === "AbortError") {
-            console.log(
-                "Retrying token retrieval due to service worker readiness issue..."
-            );
-            setTimeout(() => getFCMToken(retries - 1), 1000); // Retry after 1 second
-        } else {
-            console.error("Failed to get the token:", error);
+            if (token) {
+                console.log("FCM Token:", token);
+                // Optionally send this token to your server for future use
+            } else {
+                console.log(
+                    "No registration token available. Requesting permission."
+                );
+                await this.requestNotificationPermission();
+            }
+        } catch (error) {
+            if (retries > 0 && error.name === "AbortError") {
+                console.log("Retrying token retrieval...");
+                setTimeout(() => this.getFCMToken(retries - 1), 1000);
+            } else {
+                console.error("Failed to get FCM token:", error);
+            }
         }
     }
-}
 
-async function requestNotificationPermission() {
-    Notification.requestPermission().then((permission) => {
+    async requestNotificationPermission() {
+        const permission = await Notification.requestPermission();
         if (permission === "granted") {
             console.log("Notification permission granted.");
-            getFCMToken();
+            await this.getFCMToken();
         } else {
             console.log("Unable to get permission to notify.");
         }
-    });
+    }
+
+    listenForMessages() {
+        onMessage(this.messaging, (payload) => {
+            console.log("Message received in foreground:", payload);
+            const { title, body, icon } = payload.notification;
+            new Notification(title, { body, icon });
+        });
+    }
 }
 
-// Listen for foreground messages
-onMessage(messaging, (payload) => {
-    console.log("Message received in foreground: ", payload);
-    const notificationTitle = payload.notification.title;
-    const notificationOptions = {
-        body: payload.notification.body,
-        icon: payload.notification.icon,
-    };
-
-    // Show the notification using the browser's Notification API
-    new Notification(notificationTitle, notificationOptions);
-});
+export default Firebase;
